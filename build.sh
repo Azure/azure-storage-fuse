19b587fedde891e5d5e28508df5bc6d7dc46d352
Adam Sorrin <asorrin@microsoft.com>
2017-09-14 21:01:51 +0000

    build.sh modification

--- build.sh
+++ build.sh
@@ -6,7 +6,18 @@ BLOBFS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BLOBFS_DIR/azure-storage-cpp-light/src
 mkdir build
 cd build
+## Use cmake3 if it's available.  If not, then fallback to the default "cmake", which will hopefully be of a version > 3.5.  Otherwise, fail.
 cmake3 -DCMAKE_BUILD_TYPE=Release ..
+if [ $? -ne 0 ]
+then
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+fi 
+if [ $? -ne 0 ]
+then
+	ERRORCODE=$?
+	echo "cmake failed.  Please ensure that cmake version 3.5 or greater is installed and available."
+	exit ERRORCODE
+fi
 make
 cd $BLOBFS_DIR/blobfuse
 make
