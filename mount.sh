e40a4afaf3799e5dd05099256132ebb39805a8c5
JasonYang-MSFT <yabo@microsoft.com>
2017-09-18 02:15:24 +0800

    Added connection.cfg and removed allow_other from mount.sh

--- mount.sh
+++ mount.sh
@@ -2,4 +2,4 @@
 BLOBFS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$BLOBFS_DIR/azure-storage-cpp-light/src/build
 cd $BLOBFS_DIR/blobfuse
-./blobfuse $1 --tmpPath=/mnt/blobfusetmp -o allow_other -o big_writes -o max_read=131072 -o max_write=131072 -o attr_timeout=240 -o fsname=blobfuse -o kernel_cache -o entry_timeout=240 -o negative_timeout=120 --configFile=connection.cfg
+./blobfuse $1 --tmpPath=/mnt/blobfusetmp -o big_writes -o max_read=131072 -o max_write=131072 -o attr_timeout=240 -o fsname=blobfuse -o kernel_cache -o entry_timeout=240 -o negative_timeout=120 --configFile=connection.cfg
