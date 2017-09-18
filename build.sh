2751e09b125df9cb1d54fa9acaeb7613c4f3f324
JasonYang-MSFT <yabo@microsoft.com>
2017-09-18 07:56:47 +0000

    Fixed build and mount script.

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ BLOBFS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ##install pkg-config, cmake, libcurl and libfuse first
 ## For example, on ubuntu - sudo apt-get install pkg-config libfuse-dev cmake libcurl4-openssl-dev -y
-cd $BLOBFS_DIR/azure-storage-cpp-light/src
 mkdir build
 cd build
 ## Use cmake3 if it's available.  If not, then fallback to the default "cmake", which will hopefully be of a version > 3.5.  Otherwise, fail.
@@ -18,6 +17,4 @@ then
 	echo "cmake failed.  Please ensure that cmake version 3.5 or greater is installed and available."
 	exit ERRORCODE
 fi
-make
-cd $BLOBFS_DIR/blobfuse
 make
--- mount.sh
+++ mount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 BLOBFS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export LD_LIBRARY_PATH=$BLOBFS_DIR/azure-storage-cpp-light/src/build
-cd $BLOBFS_DIR/blobfuse
+cd $BLOBFS_DIR/build
 ./blobfuse $1 --tmpPath=/mnt/blobfusetmp -o big_writes -o max_read=131072 -o max_write=131072 -o attr_timeout=240 -o fsname=blobfuse -o kernel_cache -o entry_timeout=240 -o negative_timeout=120 --configFile=connection.cfg
