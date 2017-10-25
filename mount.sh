9df4deca34a9bdff62ffab65f3dad5c721d9188e
Adam Sorrin <asorrin@microsoft.com>
2017-10-25 17:43:51 +0000

    mount.sh update, changing name of config file parameter

--- mount.sh
+++ mount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BLOBFS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BLOBFS_DIR/build
-./blobfuse $1 --tmpPath=/mnt/blobfusetmp -o big_writes -o max_read=131072 -o max_write=131072 -o attr_timeout=240 -o fsname=blobfuse -o kernel_cache -o entry_timeout=240 -o negative_timeout=120 --configFile=../connection.cfg
+./blobfuse $1 --tmp-path=/mnt/blobfusetmp -o big_writes -o max_read=131072 -o max_write=131072 -o attr_timeout=240 -o fsname=blobfuse -o kernel_cache -o entry_timeout=240 -o negative_timeout=120 --config-file=../connection.cfg
