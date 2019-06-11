fc4a77ed07c1692f88f9d81dcb5bb9c2f7b4a25e
amnguye <amnguye@microsoft.com>
2019-06-11 16:42:06 +0000

    Added extra check for null pointer

--- azure-storage-cpp-lite/src/tinyxml2_parser.cpp
+++ azure-storage-cpp-lite/src/tinyxml2_parser.cpp
@@ -7,9 +7,12 @@ namespace storage {
 
 std::string tinyxml2_parser::parse_text(tinyxml2::XMLElement *ele, const std::string &name) const {
     std::string text;
-    ele = ele->FirstChildElement(name.data());
-    if (ele && ele->FirstChild()) {
-        text = ele->FirstChild()->ToText()->Value();
+    if(ele)
+    {
+        ele = ele->FirstChildElement(name.data());
+        if (ele && ele->FirstChild()) {
+            text = ele->FirstChild()->ToText()->Value();
+        }
     }
 
     return text;
@@ -33,8 +36,11 @@ storage_error tinyxml2_parser::parse_storage_error(const std::string &xml) const
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XMLError::XML_SUCCESS) {
         auto xerror = xdoc.FirstChildElement("Error");
-        error.code_name = parse_text(xerror, "Code");
-        error.message = parse_text(xerror, "Message");
+        if(xerror)
+        {
+            error.code_name = parse_text(xerror, "Code");
+            error.message = parse_text(xerror, "Message");
+        }
     }
 
     return error;
@@ -45,13 +51,18 @@ list_containers_item tinyxml2_parser::parse_list_containers_item(tinyxml2::XMLEl
 
     item.name = parse_text(ele, "Name");
 
-    auto xproperty = ele->FirstChildElement("Properties");
-    item.etag = parse_text(xproperty, "Etag");
-    item.last_modified = parse_text(xproperty, "Last-Modified");
-    item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
-    item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
-    item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
-
+    if(ele)
+    {
+        auto xproperty = ele->FirstChildElement("Properties");
+        if(xproperty)
+        {
+            item.etag = parse_text(xproperty, "Etag");
+            item.last_modified = parse_text(xproperty, "Last-Modified");
+            item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
+            item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
+            item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
+        }
+    }
     //parse_metadata
 
     return item;
@@ -81,12 +92,18 @@ list_containers_response tinyxml2_parser::parse_list_containers_response(const s
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XML_SUCCESS) {
         auto xresults = xdoc.FirstChildElement("EnumerationResults");
-        response.next_marker = parse_text(xresults, "NextMarker");
-        auto xitems = xresults->FirstChildElement("Containers");
-        auto xitem = xitems->FirstChildElement("Container");
-        while (xitem) {
-            response.containers.push_back(parse_list_containers_item(xitem));
-            xitem = xitem->NextSiblingElement("Container");
+        if(xresults)
+        {
+            response.next_marker = parse_text(xresults, "NextMarker");
+            auto xitems = xresults->FirstChildElement("Containers");
+            if(xitems)
+            {
+                auto xitem = xitems->FirstChildElement("Container");
+                while (xitem) {
+                    response.containers.push_back(parse_list_containers_item(xitem));
+                    xitem = xitem->NextSiblingElement("Container");
+                }
+            }
         }
     }
 
@@ -96,21 +113,26 @@ list_containers_response tinyxml2_parser::parse_list_containers_response(const s
 list_blobs_item tinyxml2_parser::parse_list_blobs_item(tinyxml2::XMLElement *ele) const {
     list_blobs_item item;
 
-    item.name = parse_text(ele, "Name");
+    if(ele)
+    {
+        item.name = parse_text(ele, "Name");
 
-    auto xproperty = ele->FirstChildElement("Properties");
-    item.etag = parse_text(xproperty, "Etag");
-    item.last_modified = parse_text(xproperty, "Last-Modified");
-    item.cache_control = parse_text(xproperty, "Cache-Control");
-    item.content_encoding = parse_text(xproperty, "Content-Encoding");
-    item.content_language = parse_text(xproperty, "Content-Language");
-    item.content_type = parse_text(xproperty, "Content-Type");
-    item.content_md5 = parse_text(xproperty, "Content-MD5");
-    item.content_length = parse_long(xproperty, "Content-Length");
-    item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
-    item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
-    item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
-
+        auto xproperty = ele->FirstChildElement("Properties");
+        if(xproperty)
+        {
+            item.etag = parse_text(xproperty, "Etag");
+            item.last_modified = parse_text(xproperty, "Last-Modified");
+            item.cache_control = parse_text(xproperty, "Cache-Control");
+            item.content_encoding = parse_text(xproperty, "Content-Encoding");
+            item.content_language = parse_text(xproperty, "Content-Language");
+            item.content_type = parse_text(xproperty, "Content-Type");
+            item.content_md5 = parse_text(xproperty, "Content-MD5");
+            item.content_length = parse_long(xproperty, "Content-Length");
+            item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
+            item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
+            item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
+        }
+    }
     //parse_metadata
 
     return item;
@@ -122,12 +144,18 @@ list_blobs_response tinyxml2_parser::parse_list_blobs_response(const std::string
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XML_SUCCESS) {
         auto xresults = xdoc.FirstChildElement("EnumerationResults");
-        response.next_marker = parse_text(xresults, "NextMarker");
-        auto xitems = xresults->FirstChildElement("Blobs");
-        auto xitem = xitems->FirstChildElement("Blob");
-        while (xitem) {
-            response.blobs.push_back(parse_list_blobs_item(xitem));
-            xitem = xitem->NextSiblingElement("Blob");
+        if(xresults)
+        {
+            response.next_marker = parse_text(xresults, "NextMarker");
+            auto xitems = xresults->FirstChildElement("Blobs");
+            if(xitems)
+            {
+                auto xitem = xitems->FirstChildElement("Blob");
+                while (xitem) {
+                    response.blobs.push_back(parse_list_blobs_item(xitem));
+                    xitem = xitem->NextSiblingElement("Blob");
+                }
+            }
         }
     }
 
@@ -136,13 +164,16 @@ list_blobs_response tinyxml2_parser::parse_list_blobs_response(const std::string
 
 std::vector<std::pair<std::string, std::string>> tinyxml2_parser::parse_blob_metadata(tinyxml2::XMLElement *ele) const {
     std::vector<std::pair<std::string, std::string>> metadata;
-    tinyxml2::XMLElement *current = ele->FirstChildElement();
-    while (current)
+    if(ele)
     {
-        std::string name(current->Name());
-        std::string value(current->GetText());
-        metadata.push_back(make_pair(name, value));
-        current = current->NextSiblingElement();
+        tinyxml2::XMLElement *current = ele->FirstChildElement();
+        while (current)
+        {
+            std::string name(current->Name());
+            std::string value(current->GetText());
+            metadata.push_back(make_pair(name, value));
+            current = current->NextSiblingElement();
+        }
     }
     return metadata;
 }
@@ -150,26 +181,32 @@ std::vector<std::pair<std::string, std::string>> tinyxml2_parser::parse_blob_met
 list_blobs_hierarchical_item tinyxml2_parser::parse_list_blobs_hierarchical_item(tinyxml2::XMLElement *ele, bool is_directory) const {
     list_blobs_hierarchical_item item;
 
-    item.name = parse_text(ele, "Name");
-    item.is_directory = is_directory;
-    if (!is_directory)
+    if(ele)
     {
-        auto xproperty = ele->FirstChildElement("Properties");
-        item.etag = parse_text(xproperty, "Etag");
-        item.last_modified = parse_text(xproperty, "Last-Modified");
-        item.cache_control = parse_text(xproperty, "Cache-Control");
-        item.content_encoding = parse_text(xproperty, "Content-Encoding");
-        item.content_language = parse_text(xproperty, "Content-Language");
-        item.content_type = parse_text(xproperty, "Content-Type");
-        item.content_md5 = parse_text(xproperty, "Content-MD5");
-        item.content_length = parse_long(xproperty, "Content-Length");
-        item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
-        item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
-        item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
-        auto xmetadata = ele->FirstChildElement("Metadata");
-        if (xmetadata)
+        item.name = parse_text(ele, "Name");
+        item.is_directory = is_directory;
+        if (!is_directory)
         {
-            item.metadata = parse_blob_metadata(xmetadata);
+            auto xproperty = ele->FirstChildElement("Properties");
+            if(xproperty)
+            {
+                item.etag = parse_text(xproperty, "Etag");
+                item.last_modified = parse_text(xproperty, "Last-Modified");
+                item.cache_control = parse_text(xproperty, "Cache-Control");
+                item.content_encoding = parse_text(xproperty, "Content-Encoding");
+                item.content_language = parse_text(xproperty, "Content-Language");
+                item.content_type = parse_text(xproperty, "Content-Type");
+                item.content_md5 = parse_text(xproperty, "Content-MD5");
+                item.content_length = parse_long(xproperty, "Content-Length");
+                item.status = parse_lease_status(parse_text(xproperty, "LeaseStatus"));
+                item.state = parse_lease_state(parse_text(xproperty, "LeaseState"));
+                item.duration = parse_lease_duration(parse_text(xproperty, "LeaseDuration"));
+                auto xmetadata = ele->FirstChildElement("Metadata");
+                if (xmetadata)
+                {
+                    item.metadata = parse_blob_metadata(xmetadata);
+                }
+            }
         }
     }
     //parse_metadata
@@ -183,18 +220,24 @@ list_blobs_hierarchical_response tinyxml2_parser::parse_list_blobs_hierarchical_
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XML_SUCCESS) {
         auto xresults = xdoc.FirstChildElement("EnumerationResults");
-        response.next_marker = parse_text(xresults, "NextMarker");
-        auto xitems = xresults->FirstChildElement("Blobs");
-        auto xitem = xitems->FirstChildElement("Blob");
-        while (xitem) {
-            response.blobs.push_back(parse_list_blobs_hierarchical_item(xitem, false));
-            xitem = xitem->NextSiblingElement("Blob");
-        }
+        if(xresults)
+        {
+            response.next_marker = parse_text(xresults, "NextMarker");
+            auto xitems = xresults->FirstChildElement("Blobs");
+            if(xitems)
+            {
+                auto xitem = xitems->FirstChildElement("Blob");
+                while (xitem) {
+                    response.blobs.push_back(parse_list_blobs_hierarchical_item(xitem, false));
+                    xitem = xitem->NextSiblingElement("Blob");
+                }
 
-        auto xdir = xitems->FirstChildElement("BlobPrefix");
-        while (xdir) {
-            response.blobs.push_back(parse_list_blobs_hierarchical_item(xdir, true));
-            xdir = xdir->NextSiblingElement("BlobPrefix");
+                auto xdir = xitems->FirstChildElement("BlobPrefix");
+                while (xdir) {
+                    response.blobs.push_back(parse_list_blobs_hierarchical_item(xdir, true));
+                    xdir = xdir->NextSiblingElement("BlobPrefix");
+                }
+            }
         }
     }
 
@@ -217,18 +260,24 @@ get_block_list_response tinyxml2_parser::parse_get_block_list_response(const std
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XML_SUCCESS) {
         auto xresults = xdoc.FirstChildElement("BlockList");
-        auto xitems = xresults->FirstChildElement("CommittedBlocks");
-        auto xitem = xitems->FirstChildElement("Block");
-        while (xitem) {
-            response.committed.push_back(parse_get_block_list_item(xitem));
-            xitem = xitem->NextSiblingElement("Block");
-        }
+        if(xresults)
+        {
+            auto xitems = xresults->FirstChildElement("CommittedBlocks");
+            if(xitems)
+            {
+                auto xitem = xitems->FirstChildElement("Block");
+                while (xitem) {
+                    response.committed.push_back(parse_get_block_list_item(xitem));
+                    xitem = xitem->NextSiblingElement("Block");
+                }
 
-        xitems = xresults->FirstChildElement("UncommittedBlocks");
-        xitem = xitems->FirstChildElement("Block");
-        while (xitem) {
-            response.uncommitted.push_back(parse_get_block_list_item(xitem));
-            xitem = xitem->NextSiblingElement("Block");
+                xitems = xresults->FirstChildElement("UncommittedBlocks");
+                xitem = xitems->FirstChildElement("Block");
+                while (xitem) {
+                    response.uncommitted.push_back(parse_get_block_list_item(xitem));
+                    xitem = xitem->NextSiblingElement("Block");
+                }
+            }
         }
     }
 
@@ -250,10 +299,13 @@ get_page_ranges_response tinyxml2_parser::parse_get_page_ranges_response(const s
     tinyxml2::XMLDocument xdoc;
     if (xdoc.Parse(xml.data(), xml.size()) == tinyxml2::XML_SUCCESS) {
         auto xresults = xdoc.FirstChildElement("PageList");
-        auto xitem = xresults->FirstChildElement("PageRange");
-        while (xitem) {
-            response.pagelist.push_back(parse_get_page_ranges_item(xitem));
-            xitem = xitem->NextSiblingElement("PageRange");
+        if(xresults)
+        {
+            auto xitem = xresults->FirstChildElement("PageRange");
+            while (xitem) {
+                response.pagelist.push_back(parse_get_page_ranges_item(xitem));
+                xitem = xitem->NextSiblingElement("PageRange");
+            }
         }
     }
 
