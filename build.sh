5239526235ced23e0e323d48adecc8ed985b71c5
vibhansa-msft <vibhansa@microsoft.com>
2020-05-21 13:41:39 +0530

    Adding subtree build for cpplite

--- build.sh
+++ build.sh
@@ -8,10 +8,21 @@ if [ -n "${INCLUDE_TESTS}" ]; then
     cmake_args='-DCMAKE_BUILD_TYPE=RelWithDebInfo -DINCLUDE_TESTS=1 ..'
 fi
 
+## Build the cpplite lib first
+echo "Building the cpplite lib"
+mkdir cpplite/build.release
+cd cpplite/build.release
+cmake --build .
+cd -
+
 ## install pkg-config, cmake, libcurl and libfuse first
 ## For example, on ubuntu - sudo apt-get install pkg-config libfuse-dev cmake libcurl4-openssl-dev -y
 mkdir build
 cd build
+
+# Copy the cpplite lib here
+cp ../cpplite/build.release/*.a ./ 
+
 ## Use cmake3 if it's available.  If not, then fallback to the default "cmake".  Otherwise, fail.
 cmake3 $cmake_args
 if [ $? -ne 0 ]
