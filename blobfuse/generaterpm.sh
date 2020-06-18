59d430063e05591d7220d82b8d898b3f30350866
NaraVen <narven@microsoft.com>
2020-06-18 02:20:42 +0000

    tuning rpm, removing unnecesary flags

--- blobfuse/generaterpm.sh
+++ blobfuse/generaterpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #   Blobfuse RPM SPEC generator and packager
 #   Usage:
-#       $ ./rpmbuilder.sh [-ver apps_version] [-srcdir source_binaries_dir] [-dstdir destination_dir]
+#       $ ./rpmbuilder.sh [-ver apps_version] [-srcdir source_binaries_dir] [-distrover distro_version]
 #
 #   The above command will build an RPM package with the specified version
 #   building the files in source_binaries_dir and packaging the blobfuse binary.
@@ -9,28 +9,17 @@
 #   The RPM package will install the binaries in destination_dir on
 #   RPM installation
 
-mkdir ./rpmbuild;
-srcdir="`echo ~`./rpmbuild.";
-dstdir="./";
-
-cp ./README.md ./rpmbuild;
-cp ./LICENSE ./rpmbuild;
-cp ./build/blobfuse ./rpmbuild;
-tar -cvjSf blobfuse-$2-.tar.bz2 folder
-mkdir ./rpmbuild/SOURCES;
-mkdir ./rpmbuild/SPECS;
 
 while [ $# -gt 0 ]
 do
      case "$1" in
         -srcdir) srcdir="$2"; shift;;
-        -ver) version="$2"; shift;;
-        -dstdir) dstdir="$2"; shift;;        
+        -ver) version="$2"; shift;;      
         -distrover) distrover="$2"; shift;;
         --) shift; break;;
         -*)
                 echo >&2 \
-                "Usage: $0 [-ver version] [-srcdir source_directory] [-dstdir destination_directory]  [-distrover distro_version]"
+                "Usage: $0 [-ver version] [-srcdir source_directory] [-distrover distro_version]"
                 exit 1;;
         *)  break;;     # terminate while loop
     esac
@@ -38,9 +27,7 @@ do
 done
 
 echo "Version: " ${version};
-echo "EOS SDK Version: " ${sdkver};
 echo "Source dir: " ${srcdir};
-echo "Destination dir: " ${dstdir};
 echo "Linux distro and version:" ${distrover};
 
 # check if the rpmcontent directory exists
@@ -55,8 +42,9 @@ if [ -z "${version}" ]; then
         exit 1
 fi
 
+
 # These are needed directories for the rpmbuild package
-directories=( ./rpmbuild ./rpmbuild/SOURCES ./rpmbuild/SPECS )
+directories=( ~/rpmbuild ~/rpmbuild/SOURCES ~/rpmbuild/SPECS)
 
 echo "Building RPM package for blobfuse version" $version;
 
@@ -67,60 +55,75 @@ echo "From: " ${origdir};
 for dir in ${directories[*]}
 do
         mkdir -p $dir
+        echo "created dir" $dir
 done
+
+RPM_BUILD_ROOT=~/rpmbuild
 
 cd ${srcdir}
 echo "Creating Tar from: "${srcdir};
-tar -xcvf blobfuse-${version}.tar.gz blobfuse README.md LICENSE
+
+mkdir -p blobfuse-${version}
+cp blobfuse blobfuse-${version}/
+cp README.md blobfuse-${version}/
+cp LICENSE blobfuse-${version}/
+
+tar -cvjSf blobfuse-${version}-${distrover}.tar.bz2 blobfuse-${version}
 echo "Copying Tar to: ~/rpmbuild/SOURCES";
-cp blobfuse-${version}-${distrover}.tar.gz ~/rpmbuild/SOURCES
+mv blobfuse-${version}-${distrover}.tar.bz2 ~/rpmbuild/SOURCES
 cd -
+
 
 # prepare files for rpmbuild
 cat <<EOF >~/rpmbuild/.rpmmacros
-%_topdir   %(echo `pwd`)
+%_topdir   %(~/rpmbuild)
 %_tmppath  %{_topdir}/tmp
-%_bindir   ${dstdir}
+%_bindir    BUILDROOT
 EOF
 cat <<EOF > ~/rpmbuild/SPECS/blobfuse.spec
-%define        __spec_install_post %{nil}
-%define          debug_package %{nil}
-%define        __os_install_post %{_dbpath}/brp-compress
 
-Summary: :        FUSE adapter - Azure Storage Blobs
-Name: Blobfuse
+Summary:   FUSE adapter - Azure Storage Blobs
+Name: blobfuse
 Version: $version
-Release: $sdkver
+Release: 1
 License: MIT.
 Group: Applications/Tools
-SOURCE0 : %{name}-%{version}.tar.gz
+SOURCE0 : blobfuse-${version}-${distrover}.tar.bz2
 URL: http://github.com/Azure/azure-storage-fuse/
 
-BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root
+BuildRoot: %(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
+BuildRequires:    boost-thread
+BuildRequires:    boost-system
+BuildRequires:    boost-filesystem
 
 Requires: fuse >= 2.2.7
 
-
 %description
-%{summary}
+%{Summary}
 
 %prep
-# %setup -q
-rm -rf blobfuse-$version
-mkdir blobfuse-$version
-tar xzvf %_sourcedir/blobfuse-$version-$distrover.tar.gz -C ./blbofuse-$version
+
+%setup -q
 
 %build
-make clean
-make
+# make
+
+
+%install
+# though this has the name install this is run while building the rpm
+#rm -rf $RPM_BUILD_ROOT
+#mkdir -p $RPM_BUILD_ROOT/BUILD/blobfuse-${version}
+#install -p -m 755 blobfuse $RPM_BUILD_ROOT/BUILDROOT/blobfuse-$version-%{release}-%{_arch}
 
 %clean
+rm -rf $RPM_BUILD_ROOT
 
 %files
 %defattr(555,root,root,555)
+/usr/bin/blobfuse
 
 %changelog
-* June 10th 2020 Azure Storage DevX Team <blobfusedev@microsoft.com>
+* $(date) Blobfuse Dev blobfusedev@microsoft.com> ${version}
 - Building RPM package for Blobfuse using rpmbuilder
 
 EOF
