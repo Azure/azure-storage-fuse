ba1a18c34d6bc3ae96f328419872041f29ec1a3c
Vikas Bhansali <64532198+vibhansa-msft@users.noreply.github.com>
2022-03-10 09:18:05 +0530

    Double mount validation corrected and error string generalised (#722)
    Double mount validation corrected and error string generalised

--- cmd/mount.go
+++ cmd/mount.go
@@ -83,15 +83,15 @@ var pipelineStarted bool
 
 func (opt *mountOptions) validate(skipEmptyMount bool) error {
 	if opt.MountPath == "" {
-		return fmt.Errorf("argument error: Mount path not provided")
+		return fmt.Errorf("argument error: mount path not provided")
 	}
 
 	if _, err := os.Stat(opt.MountPath); os.IsNotExist(err) {
-		return fmt.Errorf("argument error: Mount Directory does not exists")
+		return fmt.Errorf("argument error: mount directory does not exists")
 	} else if common.IsDirectoryMounted(opt.MountPath) {
-		return fmt.Errorf("argument error: Directory is already mounted")
+		return fmt.Errorf("argument error: directory is already mounted")
 	} else if !skipEmptyMount && !common.IsDirectoryEmpty(opt.MountPath) {
-		return fmt.Errorf("argument error: Mount directory is not empty")
+		return fmt.Errorf("argument error: mount directory is not empty")
 	}
 
 	if err := common.ELogLevel.Parse(opt.Logging.LogLevel); err != nil {
