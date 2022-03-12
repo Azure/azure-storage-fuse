13b0cbc8a0d46d867b9d2c1ad4cf5c80f62aafae
Tamer Sherif <tasherif@microsoft.com>
2022-03-12 10:58:51 -0800

    python cleanup

--- resnet50_classify.py
+++ resnet50_classify.py
@@ -19,6 +19,9 @@ def classify_images(images):
     # we need to load the model within the process since we can't share a model across processes
     resnet_model = resnet50.ResNet50(weights='imagenet')
     
+    tic = time.time()
+    sys.stdout.write('starting to process {} images in this thread at time: {}\n'.format(len(images), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tic))))
+    
     for filename in images:
         # load image
         original = load_img(filename, target_size=(224, 224))
@@ -39,9 +42,10 @@ if __name__ == "__main__":
     # parse argument
     parser = argparse.ArgumentParser("classify dataset")
     parser.add_argument('-d', '--dataset', help='dataset dir path', required=True)
-    parser.add_argument('-n', '--job-name', help='name of the resnet job', required=True)
+    parser.add_argument('-n', '--job', help='name of the resnet job', required=True)
+    parser.add_argument('-p', '--procs', default=32, help='number of parallel processes', required=False)
     parser.add_argument('-lf', 
-                        '--log-file', 
+                        '--log', 
                         default="./blobfuse2-perf.log",
                         help='path of log file', 
                         required=False)
@@ -49,12 +53,14 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
     
     # create a pool of 32 threads
-    p = Pool(processes=32)
     dataset_path = args['dataset']
-    log_file_path = args['log-file']
-    job_name = args['job-name']
+    log_file_path = args['log']
+    job_name = args['job']
+    procs = args['procs']
+    p = Pool(processes=procs)
     tic = time.time()
 
+    sys.stdout.write('collecting images at time: {}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(tic))))
     # get list of files and split them in batches of 10k to be classified
     images = [os.path.join(dp, f) for dp, dn, filenames in os.walk(dataset_path) for f in filenames]
     image_subsets = list(chunks(images, 10000))
@@ -65,6 +71,9 @@ if __name__ == "__main__":
     p.join()
     
     toc=time.time()
+    sys.stdout.write('ended processing dataset at time {}\n'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(toc))))
+    sys.stdout.write('time elapsed {}\n'.format((toc-tic)))
+    
     result = {job_name:{}}
     result[job_name]['time elapsed'] = toc-tic
     result[job_name]['total images'] = len(images)
