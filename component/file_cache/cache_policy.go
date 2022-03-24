7cd883b069c59f9f0f1837412a6fb9b5367509e2
vibhansa-msft <vibhansa@microsoft.com>
2022-03-24 20:04:55 +0530

    Adding trace logs to deletefile api

--- component/file_cache/cache_policy.go
+++ component/file_cache/cache_policy.go
@@ -135,6 +135,8 @@ func getUsagePercentage(path string, maxSize float64) float64 {
 
 // Delete a given file
 func deleteFile(name string) error {
+	log.Debug("cachePolicy::deleteFile : attempting to delete %s", name)
+
 	err := os.Remove(name)
 	if err != nil && os.IsPermission(err) {
 		// File is not having delete permissions so change the mode and retry deletion
