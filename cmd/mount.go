6a4ac5be16c9f9261eacddbdb8c82fbbee8fc295
Gauri Prasad <gapra@microsoft.com>
2022-07-21 12:15:09 -0700

    Fixed bug in background mode where auth validation would be performed twice

--- cmd/mount.go
+++ cmd/mount.go
@@ -293,13 +293,17 @@ var mountCmd = &cobra.Command{
 
 		config.Set("mount-path", options.MountPath)
 
-		log.Crit("Starting Blobfuse2 Mount : %s on (%s)", common.Blobfuse2Version, common.GetCurrentDistro())
-		log.Crit("Logging level set to : %s", logLevel.String())
-		pipeline, err := internal.NewPipeline(options.Components)
-		if err != nil {
-			log.Err("Mount: error initializing new pipeline [%v]", err)
-			fmt.Println("failed to mount :", err)
-			Destroy(1)
+		var pipeline *internal.Pipeline
+
+		if !daemon.WasReborn() {
+			log.Crit("Starting Blobfuse2 Mount : %s on (%s)", common.Blobfuse2Version, common.GetCurrentDistro())
+			log.Crit("Logging level set to : %s", logLevel.String())
+			pipeline, err = internal.NewPipeline(options.Components)
+			if err != nil {
+				log.Err("Mount: error initializing new pipeline [%v]", err)
+				fmt.Println("failed to mount :", err)
+				Destroy(1)
+			}
 		}
 
 		if !options.Foreground {
