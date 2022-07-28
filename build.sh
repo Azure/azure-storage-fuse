35c809b1b6b9c9278ff4ebc63efce81dbe81cb0b
vibhansa-msft <vibhansa@microsoft.com>
2022-07-28 12:41:50 +0530

    Delete old binaries before regenerating

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-rm -rf blobfuse2
-rm -rf azure-storage-fuse
-
 if [ "$1" == "fuse2" ]
 then
     # Build blobfuse2 with fuse2
+    rm -rf blobfuse2
+    rm -rf azure-storage-fuse
     go build -tags fuse2 -o blobfuse2
 elif [ "$1" == "health" ]
 then
@@ -13,5 +12,7 @@ then
     go build -tags healthmon -o healthmon
 else
     # Build blobfuse2 with fuse3
+    rm -rf blobfuse2
+    rm -rf azure-storage-fuse
     go build -o blobfuse2
 fi
