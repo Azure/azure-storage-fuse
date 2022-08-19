748b35a18e82a7209823a504ef3837ba5d403690
vibhansa-msft <vibhansa@microsoft.com>
2022-08-19 17:00:50 +0530

    Update output to count % improvement and averages

--- test/scripts/run.sh
+++ test/scripts/run.sh
@@ -17,10 +17,39 @@ done < <(tail -n +3 ./test/scripts/test_cases.csv)
 sed -i '1s/$/ latest v2 write | latest v2 read | /' $outputPath
 sed -i '2s/$/ -- | -- |/' $outputPath
 
-./test/scripts/goparrun.sh $mntPath $tmpPath $v2configPath $outputPath
+#./test/scripts/goparrun.sh $mntPath $tmpPath $v2configPath $outputPath
 
 # Run v1
 sed -i '1s/$/ v1 write | v1 read | /' $outputPath
 sed -i '2s/$/ -- | -- |/' $outputPath
 
 ./test/scripts/cppparrun.sh $mntPath $tmpPath $v1configPath $outputPath
+
+# Calculate the % difference
+tail -n +3 $outputPath > temp.out
+
+sed -i '1s/$/ write improve | read improve | /' $outputPath
+sed -i '2s/$/ -- | -- |/' $outputPath
+count=0
+sed_line=3
+
+while IFS=\| read -r casenum case v2write v2read v1write v1read; do
+	writeDiff=$(( $v2write - $v1write ))
+	readDiff=$(( $v2read - $v1read ))
+
+    writePercent=`echo "scale=2; $writeDiff * 100 / $v1write" | bc`
+    readPercent=`echo "scale=2; $readDiff * 100 / $v1read" | bc`
+
+	echo $writePercent $readPercent
+	sed -i "${sed_line}s/$/ ${writePercent} |/" $outputPath
+	sed -i "${sed_line}s/$/ ${readPercent} |/" $outputPath
+	sed_line=$(($sed_line + 1))
+	count=$(( $count + 1))
+done < temp.out
+
+tail -n +3 $outputPath > temp.out
+totalWriteImprove=`cut -d "|" -f 7 temp.out | sed -e 's/ //g' | paste -sd+ | bc`
+totalReadImprove=`cut -d "|" -f 8 temp.out | sed -e 's/ //g' | paste -sd+ | bc`
+
+echo "| $count Test Case Average | -- | -- | -- | -- | `echo "scale=2; $totalWriteImprove / $count" | bc` | `echo "scale=2; $totalReadImprove / $count" | bc` |" >> $outputPath 
+rm -rf temp.out
