076c72f3cae9a4a1c8a8cb045c53c7ba2e0fb53f
Gauri Prasad <gapra@microsoft.com>
2022-08-24 14:19:53 -0700

    add averages

--- test/scripts/fio.sh
+++ test/scripts/fio.sh
@@ -50,9 +50,13 @@ do
 	sudo fusermount3 -u $mntPath
 
 	(( sed_line++ ))
+    blobfuse2_write_average=$(( $blobfuse2_write_average + $write_iops ))
+    blobfuse2_read_average=$(( $blobfuse2_read_average + $read_iops ))
 done
 
 sed_line=3
+blobfuse_write_average=0
+blobfuse_read_average=0
 for i in {1..5}; 
 do 
 	echo "Blobfuse Run $i"
@@ -62,8 +66,12 @@ do
 
     fio_result=$(fio --randrepeat=1 --ioengine=libaio --gtod_reduce=1 --name=test--bs=4k --iodepth=64 --readwrite=rw --rwmixread=75 --size=4G --filename=$mntPath/testfile4G)
     read_iops=$(echo $fio_result | sed -n "s/^.*read: IOPS=\s*\(\S*\),.*$/\1/p")
+    read_iops=$(echo $read_iops | tr '[:lower:]' '[:upper:]')
+    read_iops=$(echo $read_iops | numfmt --from=si)
     echo $read_iops
     write_iops=$(echo $fio_result | sed -n "s/^.*write: IOPS=\s*\(\S*\),.*$/\1/p")
+    write_iops=$(echo $write_iops | tr '[:lower:]' '[:upper:]')
+    write_iops=$(echo $write_iops | numfmt --from=si)
     echo $write_iops
 
 	sed -i "${sed_line}s/$/ ${write_iops} | ${read_iops} |/" $outputPath
@@ -72,16 +80,22 @@ do
 	sudo fusermount3 -u $mntPath
 
 	(( sed_line++ ))
+    blobfuse_write_average=$(( $blobfuse_write_average + $write_iops ))
+    blobfuse_read_average=$(( $blobfuse_read_average + $read_iops ))
 done
 
+blobfuse2_write_average=$(( $blobfuse2_write_average / 5 ))
+blobfuse2_read_average=$(( $blobfuse2_read_average / 5 ))
+blobfuse_write_average=$(( $blobfuse_write_average / 5 ))
+blobfuse_read_average=$(( $blobfuse_read_average / 5 ))
 
-# blobfuse2_average=$(( $blobfuse2_average / 2 ))
-# blobfuse_average=$(( $blobfuse_average / 2 ))
+sed -i "$5s/$/ ${blobfuse2_write_average} | ${blobfuse2_read_average} | ${blobfuse_write_average} | ${blobfuse_read_average} |/" $outputPath
 
-# sed -i "$5s/$/ ${blobfuse2_average} | ${blobfuse_average} |/" $outputPath
+# Calculate the % difference
+diff_write=$(( $blobfuse2_write_average - $blobfuse_write_average ))
+percent_write=`echo "scale=2; $diff * 100 / $blobfuse_write_average" | bc`
 
-# # Calculate the % difference
-# diff=$(( $blobfuse2_average - $blobfuse_average ))
-# percent=`echo "scale=2; $diff * 100 / $blobfuse_average" | bc`
+diff_read=$(( $blobfuse2_read_average - $blobfuse_read_average ))
+percent_read=`echo "scale=2; $diff * 100 / $blobfuse_read_average" | bc`
 
-# sed -i "6s/$/ ${percent} | |/" $outputPath
+sed -i "6s/$/ ${percent_write} | ${percent_read} |/" $outputPath
