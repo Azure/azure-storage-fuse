0a28bfba1d9cc682999f9dd5cfbcc77b88c356e4
Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
2022-08-26 10:45:48 -0700

    Added scripts to generate output conveniently (#872)

--- test/scripts/cppparrun.sh
+++ test/scripts/cppparrun.sh
@@ -1,34 +1,31 @@
-
-
 #!/bin/bash
-cnt=1
+
 mntPath=$1
 tmpPath=$2
+config=$3
+outputPath=$4
 
-sudo fusermount -u $mntPath
+sudo fusermount3 -u $mntPath
 rm -rf $mntPath/*
 sudo rm -rf $tmpPath/*
-rm results.txt
 
-
+cnt=1
+sed_line=3
 while IFS=, read -r thread count size; do
 
-	echo "--------------- Blobfuse2 Test $cnt ($thread : $count : $size)------------------------" >> results.txt
-	$3 $mntPath --tmp-path=$tmpPath --config-file=$4 --log-level=LOG_ERR -o allow_other --file-cache-timeout-in-seconds=0 --use-attr-cache=true --max-concurrency=32
+	echo "Blobfuse | $cnt ($thread threads: $count files : $size MB) |"
+	blobfuse $mntPath --tmp-path=$tmpPath --config-file=$config --log-level=LOG_ERR -o allow_other --file-cache-timeout-in-seconds=0 --use-attr-cache=true --max-concurrency=32
 	sleep 3
 	rm -rf $mntPath/*
 
-	./test/scripts/pwrite.sh $thread $count $size $mntPath
+	./test/scripts/pwrite.sh $thread $count $size $mntPath $outputPath $sed_line
 	sudo rm -rf $tmpPath/*
-	./test/scripts/pread.sh $thread $count $size $mntPath $mntPath
+	./test/scripts/pread.sh $thread $count $size $mntPath $outputPath $sed_line
 
 	rm -rf $mntPath/*
-	sudo fusermount -u $mntPath
+	sudo fusermount3 -u $mntPath
 
 	(( cnt++ ))
+	(( sed_line++ ))
 
 done < <(tail -n +3 ./test/scripts/test_cases.csv)
-
-
-
-
