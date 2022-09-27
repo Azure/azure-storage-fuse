9bc51e6a8c140cf64bea7f380d5fe78d72558218
vibhansa-msft <vibhansa@microsoft.com>
2022-09-27 17:18:03 +0530

    Correct handling of ignore open flags

--- component/libfuse/libfuse_handler_test_wrapper.go
+++ component/libfuse/libfuse_handler_test_wrapper.go
@@ -287,7 +287,7 @@ func testOpenAppendFlagDisableWritebackCache(suite *libfuseTestSuite) {
 func testOpenAppendFlagIgnoreAppendFlag(suite *libfuseTestSuite) {
 	defer suite.cleanupTest()
 	suite.cleanupTest() // clean up the default libfuse generated
-	config := "libfuse:\n  ignore-open-flag: true\n"
+	config := "libfuse:\n  ignore-open-flags: true\n"
 	suite.setupTestHelper(config) // setup a new libfuse with a custom config (clean up will occur after the test as usual)
 	suite.assert.True(suite.libfuse.ignoreOpenFlag)
 
