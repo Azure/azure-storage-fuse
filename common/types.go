8ab54d9613a0e7dc4e2e1360ce76e3c178e28607
Gauri Prasad <51212198+gapra-msft@users.noreply.github.com>
2022-12-02 11:38:45 -0800

    Upgrade blobfuse2 version to 2.0.1 (#991)

--- common/types.go
+++ common/types.go
@@ -47,7 +47,7 @@ import (
 
 // Standard config default values
 const (
-	blobfuse2Version_ = "2.0.1-preview.1"
+	blobfuse2Version_ = "2.0.1"
 
 	DefaultMaxLogFileSize = 512
 	DefaultLogFileCount   = 10
