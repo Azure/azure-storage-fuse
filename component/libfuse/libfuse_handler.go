4233f72bfc29c8750a301433a3515163086151c9
vibhansa-msft <vibhansa@microsoft.com>
2023-04-26 15:56:13 +0530

    Adding option to pass down allow_root option to libfuse

--- component/libfuse/libfuse_handler.go
+++ component/libfuse/libfuse_handler.go
@@ -106,6 +106,7 @@ func (lf *Libfuse) convertConfig() *C.fuse_options_t {
 	fuse_opts.negative_expiry = C.int(lf.negativeTimeout)
 	fuse_opts.readonly = C.bool(lf.readOnly)
 	fuse_opts.allow_other = C.bool(lf.allowOther)
+	fuse_opts.allow_root = C.bool(lf.allowRoot)
 	fuse_opts.trace_enable = C.bool(lf.traceEnable)
 	fuse_opts.non_empty = C.bool(lf.nonEmptyMount)
 	return fuse_opts
@@ -199,6 +200,10 @@ func populateFuseArgs(opts *C.fuse_options_t, args *C.fuse_args_t) (*C.fuse_opti
 
 	if opts.allow_other {
 		options += ",allow_other"
+	}
+
+	if opts.allow_root {
+		options += ",allow_root"
 	}
 
 	if opts.non_empty {
