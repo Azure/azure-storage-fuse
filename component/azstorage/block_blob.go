401d214261e8fd70e5fba9d0e2b765a0c900bfa7
vibhansa-msft <vibhansa@microsoft.com>
2023-06-29 16:18:26 +0530

    Updated as per review comments

--- component/azstorage/block_blob.go
+++ component/azstorage/block_blob.go
@@ -174,8 +174,8 @@ func (bb *BlockBlob) SetupPipeline() error {
 	log.Trace("BlockBlob::SetupPipeline : Setting up")
 	var err error
 
-	// This is a hack right now to disable the input watcher thread which continuously monitors this config to change
-	// Running this thread wild increases the CPU usage by 5% even when there is no activity in the system
+	// This is a workaround right now to disable the input watcher thread which continuously monitors below config to change
+	// Running this thread continuously increases the CPU usage by 5% even when there is no activity on blobfuse2 mount path
 	azcopyCommon.GetLifecycleMgr().EnableInputWatcher()
 
 	// Get the credential
