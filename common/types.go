49cb00877b9dda29de252c4b24652c9bb884dabe
vibhansa-msft <vibhansa@microsoft.com>
2023-07-04 12:19:55 +0530

    update version

--- common/types.go
+++ common/types.go
@@ -47,7 +47,7 @@ import (
 
 // Standard config default values
 const (
-	blobfuse2Version_ = "2.0.4"
+	blobfuse2Version_ = "2.0.5-preview.3"
 
 	DefaultMaxLogFileSize = 512
 	DefaultLogFileCount   = 10
