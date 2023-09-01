e7571e4bf8e94389826e0d6979c0d11153d1a90d
vibhansa-msft <vibhansa@microsoft.com>
2023-09-01 10:28:27 +0530

    Adding steps to publish alpine based docker image

--- docker/publishcontainer.sh
+++ docker/publishcontainer.sh
@@ -2,8 +2,17 @@
 
 ver=`../blobfuse2 --version | cut -d " " -f 3`
 image="azure-blobfuse2.$ver"
+alpine_image="azure-alpine-blobfuse2.$ver"
 
 docker login blobfuse2containers.azurecr.io --username $1 --password $2
+
+# Publish Ubn-22 container image
 docker tag $image:latest blobfuse2containers.azurecr.io/$image
 docker push blobfuse2containers.azurecr.io/$image
+
+# Publish Alpine container image
+docker tag $alpine_image:latest blobfuse2containers.azurecr.io/$alpine_image
+docker push blobfuse2containers.azurecr.io/$alpine_image
+
 docker logout blobfuse2containers.azurecr.io
+
