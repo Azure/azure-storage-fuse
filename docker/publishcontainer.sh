9bca56df46559ccf37a27cb8738486b9773f99b3
vibhansa-msft <vibhansa@microsoft.com>
2023-09-18 17:34:10 +0530

    make image tag configurable

--- docker/publishcontainer.sh
+++ docker/publishcontainer.sh
@@ -1,18 +1,13 @@
 
 
 ver=`../blobfuse2 --version | cut -d " " -f 3`
-image="azure-blobfuse2.$ver"
-mariner_image="azure-mariner-blobfuse2.$ver"
+image="azure-blobfuse2-$3.$ver"
 
 docker login blobfuse2containers.azurecr.io --username $1 --password $2
 
 # Publish Ubn-22 container image
 docker tag $image:latest blobfuse2containers.azurecr.io/$image
 docker push blobfuse2containers.azurecr.io/$image
-
-# Publish Mariner container image
-docker tag $mariner_image:latest blobfuse2containers.azurecr.io/$mariner_image
-docker push blobfuse2containers.azurecr.io/$mariner_image
 
 docker logout blobfuse2containers.azurecr.io
 
