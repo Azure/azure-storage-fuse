ea463df6d6f597e614221fd9e1601c8f4f20d7c5
vibhansa-msft <vibhansa@microsoft.com>
2023-09-19 14:27:21 +0530

    Adding steps for publish mariner x86 container image

--- docker/buildcontainer.sh
+++ docker/buildcontainer.sh
@@ -3,6 +3,7 @@
 cd ..
 echo "Building blobfuse2 with libfuse3"
 ./build.sh
+ls -l blobfuse2
 
 # As docker build can not go out of scope of this directory copy the binary here
 cd -
@@ -19,7 +20,10 @@ docker image rm $tag -f
 # Build new container image using current code
 echo "Build container for libfuse3"
 docker build -t $tag -f $1 .
- 
+
+# List all images to verify if new image is created
+docker images
+
 # Image build is executed so we can clean up temp executable from here
 rm -rf ./blobfuse2
 rm -rf 11-blobfuse2.conf blobfuse2-logrotate
