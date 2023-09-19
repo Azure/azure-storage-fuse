654041c3e54c5b2837ded3c33798cb33b00f8dce
vibhansa-msft <vibhansa@microsoft.com>
2023-09-19 15:56:31 +0530

    Run docker commands with sudo access

--- docker/buildcontainer.sh
+++ docker/buildcontainer.sh
@@ -15,19 +15,19 @@ ver=`./blobfuse2 --version | cut -d " " -f 3`
 tag="azure-blobfuse2-$2.$ver"
 
 # Cleanup older container image from docker
-docker image rm $tag -f
+sudo docker image rm $tag -f
 
 # Build new container image using current code
 echo "Build container for libfuse3"
-docker build -t $tag -f $1 .
+sudo docker build -t $tag -f $1 .
 
 # List all images to verify if new image is created
-docker images
+sudo docker images
 
 # Image build is executed so we can clean up temp executable from here
 rm -rf ./blobfuse2
 rm -rf 11-blobfuse2.conf blobfuse2-logrotate
 
 # If build was successful then launch a container instance
-status=`docker images | grep $tag`
+status=`sudo docker images | grep $tag`
 echo $status
--- docker/publishcontainer.sh
+++ docker/publishcontainer.sh
@@ -3,11 +3,11 @@
 ver=`../blobfuse2 --version | cut -d " " -f 3`
 image="azure-blobfuse2-$3.$ver"
 
-docker login blobfuse2containers.azurecr.io --username $1 --password $2
+sudo docker login blobfuse2containers.azurecr.io --username $1 --password $2
 
 # Publish Ubn-22 container image
-docker tag $image:latest blobfuse2containers.azurecr.io/$image
-docker push blobfuse2containers.azurecr.io/$image
+sudo docker tag $image:latest blobfuse2containers.azurecr.io/$image
+sudo docker push blobfuse2containers.azurecr.io/$image
 
-docker logout blobfuse2containers.azurecr.io
+sudo docker logout blobfuse2containers.azurecr.io
 
