43a7447cc1a0cb554522866e8f9fe533660097a9
vibhansa-msft <vibhansa@microsoft.com>
2023-11-07 10:08:19 +0530

    Adding script to create data set

--- test/scripts/fio_auto.sh
+++ test/scripts/fio_auto.sh
@@ -1,46 +1,79 @@
 #!/bin/bash
+# ./fio_auto.sh <mnt path> <1:create data>
 
-# for i in {1024,2048,4096,5192,102400,519200,1024000}; do dd if=/dev/urandom of=./$i.data bs=1M count=$i; done
 
 mntPath=$1
 
-while IFS=, read -r thread block file; do
-    echo "
-    [global]
-    ioengine=sync
-    size=${file}M
-    bs=${block}M
-    rw=read
-    filename=$mntPath/$file.data
-    numjobs=$thread
-    [job]
-    name=seq_read" > fio_temp.cfg
+#------------------------------------------------------------------------------------------------------------------
+# Create the data set
+if [[ $2 == 1 ]]
+then
+    echo "Going for data creation"
+    v2configPath="config_block.yaml"
+    ./blobfuse2 mount $mntPath --config-file=$v2configPath 
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 
-    for i in {1..3}; 
-    do 
-        echo "Blobfuse2 Run $i with $thread threads, $block block size, $file file size"
-        
-        # Mount Blobfuse2
-        ./blobfuse2 mount $mntPath --config-file=$v2configPath &
-        if [ $? -ne 0 ]; then
-            exit 1
-        fi
+    # Wait for mount to stablize
+    sleep 3
 
-        # Wait for mount to stablize
-        sleep 3
+    # Print process summary to validte blobfuse2 is indeed mounted
+    ps -aux | grep blobfuse2
 
-        # Print process summary to validte blobfuse2 is indeed mounted
-        ps -aux | grep blobfuse2
-
-        # Run sequential read test
-        time fio fio_temp.cfg --output fio_result_$file$i.csv --output-format csv
-
-        # Unmount Blobfuse2
-        ./blobfuse2 unmount all
-
+    for file in $(cat fio_tests.csv  | cut -d "," -f3 | tail -n +3 | sort -u);
+    do
+        echo "Creating: " $file
+        dd if=/dev/urandom of=$mntPath/$file.data bs=1M count=$file
     done
+    ./blobfuse2 unmount all
+else
+    echo "Skipping data creation"
+fi
+#------------------------------------------------------------------------------------------------------------------
 
-done < <(tail -n +3 ./test/scripts/fio_tests.csv)
+
+# Execute the Sequential read FIO test
+for v2configPath in "config_block.yaml" "config_file.yaml";
+do
+    while IFS=, read -r thread block file; do
+        echo "
+        [global]
+        ioengine=sync
+        size=${file}M
+        bs=${block}M
+        rw=read
+        filename=$mntPath/$file.data
+        numjobs=$thread
+        [job]
+        name=seq_read" > fio_temp.cfg
+
+        for i in {1..3}; 
+        do 
+            echo "Blobfuse2 Run $i with $thread threads, $block block size, $file file size"
+            
+            # Mount Blobfuse2
+            ./blobfuse2 mount $mntPath --config-file=$v2configPath
+            if [ $? -ne 0 ]; then
+                exit 1
+            fi
+
+            # Wait for mount to stablize
+            sleep 3
+
+            # Print process summary to validte blobfuse2 is indeed mounted
+            ps -aux | grep blobfuse2
+
+            # Run sequential read test
+            time fio fio_temp.cfg --output fio_result_$file$i.csv --output-format csv
+
+            # Unmount Blobfuse2
+            ./blobfuse2 unmount all
+
+        done
+
+    done < <(tail -n +3 ./test/scripts/fio_tests.csv)
+done
 
 cat fio_result_*.csv 
 
