5f5a7a678acd23bfa40c59ba044a2e35c8e29e62
vibhansa-msft <vibhansa@microsoft.com>
2023-11-22 14:41:54 +0530

    Correcting script

--- test/scripts/file_block_compare.sh
+++ test/scripts/file_block_compare.sh
@@ -28,26 +28,27 @@ rm -rf $tmpPath/*
 
 # ------------------------------------------------------------------------------------------------------------------
 # Create the data set
-if [[ $2 == 1 ]]
-then
-    # Generate report format
-    echo "Going for data creation"
-    outputPath="./file_block_write.txt"
-    echo "| File Size (MB) | Block Cache Speed | Block Cache Time | File Cache Speed | File Cache Time |" > $outputPath
-    echo "| -- | -- | -- | -- | -- |" >> $outputPath
-    
-    # Fill the test case data
+
+# Generate report format
+echo "Going for data creation"
+outputPath="./file_block_write.txt"
+echo "| File Size (MB) | Block Cache Speed | Block Cache Time | File Cache Speed | File Cache Time |" > $outputPath
+echo "| -- | -- | -- | -- | -- |" >> $outputPath
+
+# Fill the test case data
+for file in $(cat ./test/scripts/fio_tests.csv  | cut -d "," -f3 | tail -n +3 | sort -u);
+do
+    echo "| ${file} |" >> $outputPath
+done
+
+# Execute the data creation dd test with block-size=32MB
+for v2configPath in $blockConfigPath $fileConfigPath;
+do
+    sed_line=3
+    echo "Running creation with $v2configPath"
+
     for file in $(cat ./test/scripts/fio_tests.csv  | cut -d "," -f3 | tail -n +3 | sort -u);
     do
-        echo "| ${file} |" >> $outputPath
-    done
-    
-    # Execute the data creation dd test with block-size=32MB
-    for v2configPath in $blockConfigPath $fileConfigPath;
-    do
-        sed_line=3
-
-        echo "Running creation with $v2configPath"
         ./blobfuse2 mount $mntPath --config-file=$v2configPath --tmp-path=$tmpPath --file-cache-timeout=0
         if [ $? -ne 0 ]; then
             exit 1
@@ -56,27 +57,70 @@ then
         # Wait for mount to stabilize
         sleep 3
 
-        for file in $(cat ./test/scripts/fio_tests.csv  | cut -d "," -f3 | tail -n +3 | sort -u);
-        do
-            echo "Creating: " $file
-            dd if=/dev/urandom of=$mntPath/$dataPath/$v2configPath$file.data bs=1M count=$file 2> temp.tst
-            write_speed=`cat temp.tst | tail -1 | rev | cut -d " " -f1,2 | rev | cut -d "/" -f1`
-            write_time=`cat temp.tst | tail -1 |  cut -d "," -f3`
-            
-            sed -i "${sed_line}s/$/ ${write_speed} | ${write_time} |/" $outputPath
-            (( sed_line++ ))
-        done
+        echo "Creating: " $file
+        dd if=/dev/urandom of=$mntPath/$dataPath/$v2configPath$file.data bs=1M count=$file 2> temp.tst
+        write_speed=`cat temp.tst | tail -1 | rev | cut -d " " -f1,2 | rev | cut -d "/" -f1`
+        write_time=`cat temp.tst | tail -1 |  cut -d "," -f3`
+        
+        sed -i "${sed_line}s/$/ ${write_speed} | ${write_time} |/" $outputPath
+        (( sed_line++ ))
+
         ./blobfuse2 unmount all
-         echo "| -- | -- | -- |" >> $outputPath
-         cat $outputPath
     done
-else
-    echo "Skipping data creation"
-fi
+done
+echo "| -- | -- | -- |" >> $outputPath
+cat $outputPath
 
 # ------------------------------------------------------------------------------------------------------------------
 
-# Read test case starts here
+# Read test with dd command
+# Generate report format
+echo "Going for Read tests with dd"
+outputPath="./file_block_read_dd.txt"
+echo "| Block Size (MB) | File Size (MB) | Block Cache Speed | Block Cache Time | File Cache Speed | File Cache Time |" > $outputPath
+echo "| -- | -- | -- | -- | -- | -- |" >> $outputPath
+
+# Generate the test case data
+while IFS=, read -r thread block file; do
+    echo "| ${block} | ${file} |" >> $outputPath
+done < <(cat ./test/scripts/fio_tests.csv  | tail -n +3 | cut -d "," -f2,3 | sort -u)
+
+# Execute the Sequential read FIO test
+for v2configPath in $blockConfigPath $fileConfigPath;
+do
+    sed_line=3
+    echo "Running read test with $v2configPath"
+
+    while IFS=, read -r block file; do
+        echo "Blobfuse2 Run with $block block size, $file file size"
+        
+        # Mount Blobfuse2
+        ./blobfuse2 mount $mntPath --config-file=$v2configPath --tmp-path=$tmpPath --file-cache-timeout=0
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+
+        # Wait for mount to stabilize
+        sleep 3
+
+        dd of=/dev/null if=$mntPath/$dataPath/$v2configPath$file.data bs=${block}M count=$file 2> temp.tst
+        read_speed=`cat temp.tst | tail -1 | rev | cut -d " " -f1,2 | rev | cut -d "/" -f1`
+        read_time=`cat temp.tst | tail -1 |  cut -d "," -f3`
+
+        sed -i "${sed_line}s/$/ ${read_bw} | ${read_time} |/" $outputPath
+        (( sed_line++ ))
+
+        # Unmount Blobfuse2
+        ./blobfuse2 unmount all
+    done < <(cat ./test/scripts/fio_tests.csv  | tail -n +3 | cut -d "," -f2,3 | sort -u)
+done
+echo "| -- | -- | -- | -- | -- | -- |" >> $outputPath
+cat $outputPath
+
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Read test case with fio command
 # Generate report format
 echo "Going for Read tests"
 outputPath="./file_block_read.txt"
@@ -131,5 +175,3 @@ done
 echo "| -- | -- | -- | -- | -- | -- | -- |" >> $outputPath
 cat $outputPath
 # ------------------------------------------------------------------------------------------------------------------
-
-
