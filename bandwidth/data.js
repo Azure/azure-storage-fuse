3548d590b072bb16df2e71f3e0813bb0156fdbe5
github-action-benchmark <github@users.noreply.github.com>
2024-03-01 11:39:26 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f06c947e60290cab68e4e3ae83f4b7a14b8be5d9

---
+++ bandwidth/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709293166901,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "f06c947e60290cab68e4e3ae83f4b7a14b8be5d9",
+          "message": "Updated",
+          "timestamp": "2024-03-01T17:01:01+05:30",
+          "tree_id": "c9f02eb92fa5ee6f665472e6dd8ae68740b9cead",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/f06c947e60290cab68e4e3ae83f4b7a14b8be5d9"
+        },
+        "date": 1709293164650,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write",
+            "value": 1302.7340494791667,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
