94ea05145f0e73ba892ffd70776fc545599804b8
github-action-benchmark <github@users.noreply.github.com>
2024-03-06 07:46:23 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fda6a3ce221e51de94abad16b0bbc5259c12caaf

---
+++ bandwidth/highlyparallel/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709711183463,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "fda6a3ce221e51de94abad16b0bbc5259c12caaf",
+          "message": "Renaming config files to create order in graphs",
+          "timestamp": "2024-03-06T12:18:31+05:30",
+          "tree_id": "ee803bc621b695aa2bf7ac0a6382c58327f07583",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/fda6a3ce221e51de94abad16b0bbc5259c12caaf"
+        },
+        "date": 1709711183154,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "seq_write_112_thread",
+            "value": 33954.844075520836,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "seq_read_128_thread",
+            "value": 0,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "rand_read_128_thread",
+            "value": 0,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
