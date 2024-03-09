1fee94044d958467cb0314faf31e30f970a4645a
github-action-benchmark <github@users.noreply.github.com>
2024-03-09 07:59:53 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 087c53613e3d10c6c2ab16b4eb5f31f52cf9816f

--- bandwidth/write/data.js
+++ bandwidth/write/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709872444579,
+  "lastUpdate": 1709971193719,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -135,6 +135,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "sequential_write_directio",
             "value": 1671.9104817708333,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "087c53613e3d10c6c2ab16b4eb5f31f52cf9816f",
+          "message": "Correcting alignment",
+          "timestamp": "2024-03-09T13:19:56+05:30",
+          "tree_id": "f24e1f0e70fd7d41f2db68f3f60a6de076812c1c",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/087c53613e3d10c6c2ab16b4eb5f31f52cf9816f"
+        },
+        "date": 1709971192643,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write",
+            "value": 1741.1334635416667,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 1899.26953125,
             "unit": "MiB/s"
           }
         ]
