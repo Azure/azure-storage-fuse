18471d4518af829b54a521350a34ba60a20936df
github-action-benchmark <github@users.noreply.github.com>
2024-03-09 09:14:54 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ac41490297dbd1e480710cfefbdd88926e56f57a

---
+++ latency/write/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709975694546,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "ac41490297dbd1e480710cfefbdd88926e56f57a",
+          "message": "Correcting file name",
+          "timestamp": "2024-03-09T14:35:07+05:30",
+          "tree_id": "e28189d0bbc4657d9ea2adb5d26dde1e208e3344",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/ac41490297dbd1e480710cfefbdd88926e56f57a"
+        },
+        "date": 1709975694236,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write",
+            "value": 0.5075749762,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write",
+            "value": 0.499404268442,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write",
+            "value": 0.500775125343,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 0.561904883938,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 0.56342317296,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 0.549992684148,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
