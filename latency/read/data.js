3a12b4c9c08479eec3b6a34f61b83c698f80ded3
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 17:38:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b8e905041b73f9a1a1b6e9d8c20adccde8061ff5

--- latency/read/data.js
+++ latency/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710434160965,
+  "lastUpdate": 1710437906730,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -68,6 +68,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "random_read_four_threads",
             "value": 72.506766126339,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "b8e905041b73f9a1a1b6e9d8c20adccde8061ff5",
+          "message": "Correcting condition",
+          "timestamp": "2024-03-14T22:28:41+05:30",
+          "tree_id": "761b9036e9e7ecefa4bff1acfa88658816f1f82b",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/b8e905041b73f9a1a1b6e9d8c20adccde8061ff5"
+        },
+        "date": 1710437906084,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read",
+            "value": 1.8760104535266666,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read",
+            "value": 76.00654636732833,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_small_file",
+            "value": 0.40077807351700007,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 0.856094331578,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_direct_io",
+            "value": 1.9371153429066668,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_direct_io",
+            "value": 72.80917143922834,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_four_threads",
+            "value": 2.3311029756493333,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_sixteen_threads",
+            "value": 4.013385819402333,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_four_threads",
+            "value": 75.66196319188799,
             "unit": "milliseconds"
           }
         ]
