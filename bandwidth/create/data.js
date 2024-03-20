ef49770f9e234e382d731f665a68e9e18fd541a0
github-action-benchmark <github@users.noreply.github.com>
2024-03-20 08:18:17 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a34ec9f46def7c71da12fb55e24f370c72219ca4

--- bandwidth/create/data.js
+++ bandwidth/create/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710844291261,
+  "lastUpdate": 1710922697455,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "create_1l_files_in_20_threads",
             "value": 0.1123046875,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "a34ec9f46def7c71da12fb55e24f370c72219ca4",
+          "message": "Remove log printing stage",
+          "timestamp": "2024-03-20T12:13:40+05:30",
+          "tree_id": "4ea0b229bc95797534203bd46148d3d2b1608cae",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/a34ec9f46def7c71da12fb55e24f370c72219ca4"
+        },
+        "date": 1710922697118,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "create_1000_files_in_10_threads",
+            "value": 94.330078125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "create_1000_files_in_100_threads",
+            "value": 98,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "create_1l_files_in_20_threads",
+            "value": 0.0908203125,
             "unit": "MiB/s"
           }
         ]
