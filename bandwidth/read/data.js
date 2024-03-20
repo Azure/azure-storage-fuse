d104a8d8800679bb1da3730d020233722df4eab3
github-action-benchmark <github@users.noreply.github.com>
2024-03-20 07:14:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a34ec9f46def7c71da12fb55e24f370c72219ca4

--- bandwidth/read/data.js
+++ bandwidth/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710846484647,
+  "lastUpdate": 1710918871063,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -551,6 +551,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "random_read_four_threads",
             "value": 56.480794270833336,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "a34ec9f46def7c71da12fb55e24f370c72219ca4",
+          "message": "Remove log printing stage",
+          "timestamp": "2024-03-20T12:13:40+05:30",
+          "tree_id": "4ea0b229bc95797534203bd46148d3d2b1608cae",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/a34ec9f46def7c71da12fb55e24f370c72219ca4"
+        },
+        "date": 1710918870016,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read",
+            "value": 516.6832682291666,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read",
+            "value": 14.214518229166666,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_small_file",
+            "value": 2254.4020182291665,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 1265.5572916666667,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_direct_io",
+            "value": 512.236328125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_direct_io",
+            "value": 13.939453125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_four_threads",
+            "value": 1574.279296875,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_sixteen_threads",
+            "value": 4343.899088541667,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_four_threads",
+            "value": 55.269205729166664,
             "unit": "MiB/s"
           }
         ]
