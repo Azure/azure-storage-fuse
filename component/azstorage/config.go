1228f8483c536fdccf9d3f3b758fae197c253524
souravgupta <souravgupta@microsoft.com>
2024-03-21 16:04:13 +0530

    az login using object id

--- component/azstorage/config.go
+++ component/azstorage/config.go
@@ -430,6 +430,7 @@ func ParseAndValidateConfig(az *AzStorage, opt AzStorageOptions) error {
 	}
 
 	az.stConfig.authConfig.ObjectID = opt.ObjectID
+	az.stConfig.authConfig.TenantID = opt.TenantID
 
 	switch authType {
 	case EAuthType.KEY():
@@ -460,11 +461,9 @@ func ParseAndValidateConfig(az *AzStorage, opt AzStorageOptions) error {
 		}
 		az.stConfig.authConfig.ClientID = opt.ClientID
 		az.stConfig.authConfig.ClientSecret = opt.ClientSecret
-		az.stConfig.authConfig.TenantID = opt.TenantID
 		az.stConfig.authConfig.OAuthTokenFilePath = opt.OAuthTokenFilePath
 	case EAuthType.AZCLI():
 		az.stConfig.authConfig.AuthMode = EAuthType.AZCLI()
-		az.stConfig.authConfig.TenantID = opt.TenantID
 
 	default:
 		log.Err("ParseAndValidateConfig : Invalid auth mode %s", opt.AuthMode)
