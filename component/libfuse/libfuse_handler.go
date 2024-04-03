3ad88c62f1b343b86f9b3dfb579d538a84e873f3
vibhansa <vibhansa@microsoft.com>
2024-04-03 11:32:28 +0530

    Set value

--- component/libfuse/libfuse_handler.go
+++ component/libfuse/libfuse_handler.go
@@ -323,11 +323,12 @@ func libfuse_init(conn *C.fuse_conn_info_t, cfg *C.fuse_config_t) (res unsafe.Po
 	// RHEL still has 3.3 fuse version and it does not allow max_write beyond 128K
 	// Setting this value to 1 MB will fail the mount.
 	fuse_minor := C.get_fuse_minor_version()
-	if fuse_minor > 5 {
+	if fuse_minor > 4 {
 		log.Info("Libfuse::libfuse_init : Setting 1MB max_write for fuse minor %v", fuse_minor)
 		conn.max_write = (1 * 1024 * 1024)
 	} else {
 		log.Info("Libfuse::libfuse_init : Ignoring max_write for fuse minor %v", fuse_minor)
+		conn.max_write = (128 * 1024)
 	}
 
 	// direct_io option is used to bypass the kernel cache. It disables the use of
