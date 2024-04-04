7fdb895594f7ad70aa9a5d112f12c3289ba08a60
github-action-benchmark <github@users.noreply.github.com>
2024-04-04 11:45:31 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1a4e554337ce8799974951b862bc67522031adf1

---
+++ latency/create/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1712231131651,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "1a4e554337ce8799974951b862bc67522031adf1",
+          "message": "Correcting bs in large write case",
+          "timestamp": "2024-04-04T15:58:04+05:30",
+          "tree_id": "4a43bfe9042ae83dab8725a2ba1ea42ed150b950",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/1a4e554337ce8799974951b862bc67522031adf1"
+        },
+        "date": 1712231131307,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "create_1000_files_in_10_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "create_1000_files_in_100_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "create_1l_files_in_20_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
