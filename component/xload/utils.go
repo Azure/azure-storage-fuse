cad57567afcdf7159eec940890c542c13215d74f
Sourav Gupta <98318303+souravgupta-msft@users.noreply.github.com>
2024-04-16 11:00:14 +0530

    changing mode type to enum (#1386)

---
+++ component/xload/utils.go
@@ -0,0 +1,77 @@
+/*
+    _____           _____   _____   ____          ______  _____  ------
+   |     |  |      |     | |     | |     |     | |       |            |
+   |     |  |      |     | |     | |     |     | |       |            |
+   | --- |  |      |     | |-----| |---- |     | |-----| |-----  ------
+   |     |  |      |     | |     | |     |     |       | |       |
+   | ____|  |_____ | ____| | ____| |     |_____|  _____| |_____  |_____
+
+
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+
+   Copyright © 2020-2024 Microsoft Corporation. All rights reserved.
+   Author : <blobfusedev@microsoft.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE
+*/
+
+package xload
+
+import (
+	"reflect"
+
+	"github.com/JeffreyRichter/enum/enum"
+)
+
+// xload mode enum
+type Mode int
+
+var EMode = Mode(0).INVALID_MODE()
+
+func (Mode) INVALID_MODE() Mode {
+	return Mode(0)
+}
+
+func (Mode) CHECKPOINT() Mode {
+	return Mode(1)
+}
+
+func (Mode) DOWNLOAD() Mode {
+	return Mode(2)
+}
+
+func (Mode) UPLOAD() Mode {
+	return Mode(3)
+}
+
+func (Mode) SYNC() Mode {
+	return Mode(4)
+}
+
+func (m Mode) String() string {
+	return enum.StringInt(m, reflect.TypeOf(m))
+}
+
+func (m *Mode) Parse(s string) error {
+	enumVal, err := enum.ParseInt(reflect.TypeOf(m), s, true, false)
+	if enumVal != nil {
+		*m = enumVal.(Mode)
+	}
+	return err
+}
