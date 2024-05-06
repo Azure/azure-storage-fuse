a7b545fc74c22502776a737ce2ae6e4a991ed89c
souravgupta <souravgupta@microsoft.com>
2024-05-06 16:53:37 +0530

    sample

---
+++ component/xload/xcomponent.go
@@ -0,0 +1,48 @@
+package xload
+
+import "github.com/Azure/azure-storage-fuse/v2/internal"
+
+type xcomponent interface {
+	init()
+	start()
+	stop()
+	process(item *workItem) (int, error)
+	getNext() xcomponent
+	setNext(s xcomponent)
+}
+
+type xbase struct {
+	remote internal.Component
+	next   xcomponent
+}
+
+var _ xcomponent = &xbase{}
+
+func (xb *xbase) init() {
+}
+
+func (xb *xbase) start() {
+}
+
+func (xb *xbase) stop() {
+}
+
+func (xb *xbase) process(item *workItem) (int, error) {
+	return 0, nil
+}
+
+func (xb *xbase) getNext() xcomponent {
+	return xb.next
+}
+
+func (xb *xbase) setNext(s xcomponent) {
+	xb.next = s
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+
+type xlister struct {
+}
+
+type xsplitter struct {
+}
