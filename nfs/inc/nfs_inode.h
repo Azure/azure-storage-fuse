afdd3d5d274c4e8ce1bb849b95e410f74648a904
Amulya Nataraj <amn@microsoft.com>
2024-05-29 15:42:44 +0000

    Add missing files

---
+++ nfs/inc/nfs_inode.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "aznfsc.h"
+
+struct nfs_inode
+{
+    // Nfs filehandle returned by the server.
+    nfs_fh3 fh;
+
+    // TODO: Add blob info structure.
+
+    //inode_number;
+    fuse_ino_t inode;
+
+    nfs_inode(const struct nfs_fh3* filehandle):
+        inode(0)
+    {
+        fh.data.data_val = new char[filehandle->data.data_len];
+        fh.data.data_len = filehandle->data.data_len;
+        ::memcpy(fh.data.data_val, filehandle->data.data_val, fh.data.data_len);
+    }
+
+    void set_inode(fuse_ino_t ino)
+    {
+        inode = ino;
+    }
+
+    const struct nfs_fh3& get_fh() const
+    {
+        return fh;
+    }
+};
