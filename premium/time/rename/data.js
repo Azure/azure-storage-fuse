c28e90aafd90ff98d8e818e2888216a4a2e984b4
github-action-benchmark <github@users.noreply.github.com>
2024-06-27 11:52:05 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for eb76cd1603914937829d87512f5ae46f03bb139d

---
+++ premium/time/rename/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719489125951,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "eb76cd1603914937829d87512f5ae46f03bb139d",
+          "message": "Updated",
+          "timestamp": "2024-06-27T03:26:05-07:00",
+          "tree_id": "59cb8c935adfa5c3dc8d51a5df4ae9fed760a198",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/eb76cd1603914937829d87512f5ae46f03bb139d"
+        },
+        "date": 1719489125651,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rename_5000_1MB_files",
+            "value": 113.07432961463928,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
