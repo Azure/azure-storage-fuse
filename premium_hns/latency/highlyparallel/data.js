1dc75924494432c361a77a36d4fd4a93ac18a2fe
github-action-benchmark <github@users.noreply.github.com>
2024-07-01 19:16:45 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 233235c3c0d20b6a63516920f18cb331c3f07302

---
+++ premium_hns/latency/highlyparallel/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719861405614,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "233235c3c0d20b6a63516920f18cb331c3f07302",
+          "message": "updated",
+          "timestamp": "2024-07-01T04:09:13-07:00",
+          "tree_id": "ee1d47c48275271aa297e35d238317578db6cbb2",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/233235c3c0d20b6a63516920f18cb331c3f07302"
+        },
+        "date": 1719861405380,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "seq_write_112_thread",
+            "value": 247.0737843157633,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "seq_read_128_thread",
+            "value": 406.12927319571503,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "rand_read_128_thread",
+            "value": 1545.356599545389,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
