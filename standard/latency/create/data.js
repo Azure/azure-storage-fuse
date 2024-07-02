f603a14d9057e9b506cd04197ec37950a8b60ce7
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 18:59:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 559f3a7a508cbaf394bdcaea19e2868eeee8e81d

---
+++ standard/latency/create/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719946768438,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "559f3a7a508cbaf394bdcaea19e2868eeee8e81d",
+          "message": "Updated",
+          "timestamp": "2024-07-02T04:06:54-07:00",
+          "tree_id": "5049cd24972625012757215aec88556fa6c492f1",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/559f3a7a508cbaf394bdcaea19e2868eeee8e81d"
+        },
+        "date": 1719946768199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "create_1000_files_in_10_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "create_1000_files_in_100_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "create_1l_files_in_20_threads",
+            "value": 0,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
