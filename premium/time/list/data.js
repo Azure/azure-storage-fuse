6fd9fde120961648187e3aacbac4894f6f32fd5f
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 01:32:47 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 98acac361ff7a594d3e2bc18f2eef0d611e055c2

--- premium/time/list/data.js
+++ premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720563735766,
+  "lastUpdate": 1720575167237,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -263,6 +263,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "430.30000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ashruti-msft",
+            "username": "ashruti-msft",
+            "email": "137055338+ashruti-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "98acac361ff7a594d3e2bc18f2eef0d611e055c2",
+          "message": "Added min prefetch check (#1446)\n\n* Added check for memsize and prefetch if set by default",
+          "timestamp": "2024-07-09T10:23:58Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/98acac361ff7a594d3e2bc18f2eef0d611e055c2"
+        },
+        "date": 1720575167001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "22.61000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "419.05000",
             "unit": "seconds"
           }
         ]
