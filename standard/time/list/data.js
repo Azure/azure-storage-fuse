fffa1a12aa65d6fda095498dccbf6582e36a7864
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 10:23:15 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8f767d0251fc23bddb7dc73f3a2a8e792f39412d

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720596566147,
+  "lastUpdate": 1720606995405,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -361,6 +361,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "539.63000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft",
+            "email": "64532198+vibhansa-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "8f767d0251fc23bddb7dc73f3a2a8e792f39412d",
+          "message": "Remove RHEL 7.5 from nightly and artifacts. (#1448)\n\n* Remove RHLE7.5 from nightly and artifact tests.",
+          "timestamp": "2024-07-10T07:11:56Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/8f767d0251fc23bddb7dc73f3a2a8e792f39412d"
+        },
+        "date": 1720606995112,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "22.45000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "509.96000",
             "unit": "seconds"
           }
         ]
