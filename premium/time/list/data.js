be9b3c3a31c27a78074ae09ef061c452385d9595
github-action-benchmark <github@users.noreply.github.com>
2024-07-12 04:17:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f088b803fe387bbb1f5f76caedbe75cf2439b003

--- premium/time/list/data.js
+++ premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720747790119,
+  "lastUpdate": 1720757844136,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -811,6 +811,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "358.09000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ashruti-msft",
+            "username": "ashruti-msft",
+            "email": "137055338+ashruti-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f088b803fe387bbb1f5f76caedbe75cf2439b003",
+          "message": "Fixed block-cache test (#1454)\n\n* Fix UT for prefetch count",
+          "timestamp": "2024-07-10T11:17:29Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/f088b803fe387bbb1f5f76caedbe75cf2439b003"
+        },
+        "date": 1720757843861,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "30.19000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "379.85000",
             "unit": "seconds"
           }
         ]
