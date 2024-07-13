619cd7fcc5fc885885cecd269cb2768f6d16de4a
github-action-benchmark <github@users.noreply.github.com>
2024-07-13 07:33:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7f395591dbea6264df3160b99c37fbaf4baea1dd

--- standard/time/app/data.js
+++ standard/time/app/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720845146064,
+  "lastUpdate": 1720856006706,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -689,6 +689,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "read_40GB",
             "value": 20.772731065750122,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ashruti-msft",
+            "username": "ashruti-msft",
+            "email": "137055338+ashruti-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7f395591dbea6264df3160b99c37fbaf4baea1dd",
+          "message": "ObjectID info updated and simplified base config (#1452)",
+          "timestamp": "2024-07-12T10:41:38Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/7f395591dbea6264df3160b99c37fbaf4baea1dd"
+        },
+        "date": 1720856006437,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "write_1GB",
+            "value": 2.371274709701538,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_10GB",
+            "value": 5.459805488586426,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_100GB",
+            "value": 51.30606818199158,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_40GB",
+            "value": 25.652040719985962,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_1GB",
+            "value": 0.8480255603790283,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_10GB",
+            "value": 5.237456798553467,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_100GB",
+            "value": 39.05965328216553,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_40GB",
+            "value": 16.24817681312561,
             "unit": "seconds"
           }
         ]
