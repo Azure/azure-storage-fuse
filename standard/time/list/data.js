6f6a2126709b3a8f8f3d31e5f460b0e188b07ef9
github-action-benchmark <github@users.noreply.github.com>
2024-07-23 08:21:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 73ff84985ac6e6008be4710a4df7c09b405f6d45

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721544402963,
+  "lastUpdate": 1721722862063,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -1939,6 +1939,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "468.81000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "98318303+souravgupta-msft@users.noreply.github.com",
+            "name": "Sourav Gupta",
+            "username": "souravgupta-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "73ff84985ac6e6008be4710a4df7c09b405f6d45",
+          "message": "Updating TSG for proxy environment (#1464)\n\n* adding tsg for proxy env",
+          "timestamp": "2024-07-23T11:09:28+05:30",
+          "tree_id": "0fb8febcadbaa4a76e84fa495dcb1d52b5bdee54",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/73ff84985ac6e6008be4710a4df7c09b405f6d45"
+        },
+        "date": 1721722861811,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "20.73000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "448.02000",
             "unit": "seconds"
           }
         ]
