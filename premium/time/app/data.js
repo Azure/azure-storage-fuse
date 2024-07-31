7560932829212d8428eea471212e2ff9c146053e
github-action-benchmark <github@users.noreply.github.com>
2024-07-31 06:47:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for da781335200793f51bbd626f5dfd553009f91cd7

--- premium/time/app/data.js
+++ premium/time/app/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722341732300,
+  "lastUpdate": 1722408476094,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -3629,6 +3629,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "read_40GB",
             "value": 18.452930212020874,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137055338+ashruti-msft@users.noreply.github.com",
+            "name": "ashruti-msft",
+            "username": "ashruti-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "da781335200793f51bbd626f5dfd553009f91cd7",
+          "message": "Block Cache Read correction  (#1483)\n\nFixed: We copied the entire block regardless of whether it was fully used, leading to copying over garbage data.\r\nFixed: Error in read when disk cache was enabled",
+          "timestamp": "2024-07-31T11:00:28+05:30",
+          "tree_id": "504b3aca1f21642ba9f35299b2e1474afdb4fc8e",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/da781335200793f51bbd626f5dfd553009f91cd7"
+        },
+        "date": 1722408475857,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "write_1GB",
+            "value": 1.4187805652618408,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_10GB",
+            "value": 5.638704776763916,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_100GB",
+            "value": 51.1437931060791,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_40GB",
+            "value": 21.57042121887207,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_1GB",
+            "value": 0.7645406723022461,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_10GB",
+            "value": 4.039325952529907,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_100GB",
+            "value": 51.90732669830322,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_40GB",
+            "value": 20.628644943237305,
             "unit": "seconds"
           }
         ]
