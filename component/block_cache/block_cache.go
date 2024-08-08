4f2685d1489be135e574b56a3ceb3138f3eb60bb
vibhansa <vibhansa@microsoft.com>
2024-08-07 21:58:22 -0700

    Close channel only if its open

--- component/block_cache/block_cache.go
+++ component/block_cache/block_cache.go
@@ -1233,10 +1233,14 @@ func (bc *BlockCache) waitAndFreeUploadedBlocks(handle *handlemap.Handle, cnt in
 		block := node.Value.(*Block)
 		if block.id != -1 {
 			// Wait for upload of this block to complete
-			<-block.state
+			_, ok := <-block.state
 			block.flags.Clear(BlockFlagUploading)
+			if ok {
+				block.Unblock()
+			}
+		} else {
+			block.Unblock()
 		}
-		block.Unblock()
 
 		if block.IsFailed() {
 			log.Err("BlockCache::waitAndFreeUploadedBlocks : Failed to upload block, posting back to cooking list %v=>%s (index %v, offset %v)", handle.ID, handle.Path, block.id, block.offset)
