2d8c34e9f936448af7242b9be1c205df7633e167
github-action-benchmark <github@users.noreply.github.com>
2024-09-01 06:49:30 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d06331738096999b358a430fb339e11dcd4d7642

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725019578830,
+  "lastUpdate": 1725173370632,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -2343,6 +2343,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "450.70000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Sourav Gupta",
+            "username": "souravgupta-msft",
+            "email": "98318303+souravgupta-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d06331738096999b358a430fb339e11dcd4d7642",
+          "message": "Added block cache limitations (#1511)",
+          "timestamp": "2024-08-30T09:23:53Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/d06331738096999b358a430fb339e11dcd4d7642"
+        },
+        "date": 1725173370383,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "13.51000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "493.66000",
             "unit": "seconds"
           }
         ]
