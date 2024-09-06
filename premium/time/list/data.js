49d37fb3eb5eeb5bdffb05ef2f152e980eca75a9
github-action-benchmark <github@users.noreply.github.com>
2024-09-06 11:20:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for a42da599c0d65e83577b4235ef0e581e68bd06b3

---
+++ premium/time/list/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1725621655582,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "syeleti@microsoft.com",
+            "name": "syeleti-msft",
+            "username": "syeleti-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a42da599c0d65e83577b4235ef0e581e68bd06b3",
+          "message": "Making pipeline consistent with the units (#1461)",
+          "timestamp": "2024-09-06T15:37:46+05:30",
+          "tree_id": "dec09c4da4dcd8fa7bdf1922c2214e2050ee087e",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/a42da599c0d65e83577b4235ef0e581e68bd06b3"
+        },
+        "date": 1725621655330,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "13.60000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "350.39000",
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
