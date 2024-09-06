3ac3a13da3d0d27ef49631e9ec7380e615f63ec6
github-action-benchmark <github@users.noreply.github.com>
2024-09-06 11:26:26 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a42da599c0d65e83577b4235ef0e581e68bd06b3

---
+++ premium/bandwidth/highapp/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1725621986138,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "syeleti@microsoft.com",
+            "name": "syeleti-msft",
+            "username": "syeleti-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a42da599c0d65e83577b4235ef0e581e68bd06b3",
+          "message": "Making pipeline consistent with the units (#1461)",
+          "timestamp": "2024-09-06T15:37:46+05:30",
+          "tree_id": "dec09c4da4dcd8fa7bdf1922c2214e2050ee087e",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/a42da599c0d65e83577b4235ef0e581e68bd06b3"
+        },
+        "date": 1725621985907,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "read_10_20GB_file",
+            "value": 3.228625018863306,
+            "unit": "GiB/s"
+          },
+          {
+            "name": "create_10_20GB_file",
+            "value": 3.7058894990651283,
+            "unit": "GiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
