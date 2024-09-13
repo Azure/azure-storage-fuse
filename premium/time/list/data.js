7a1b0dfba135f8d2a4cd215ee1d53a3d7f660a82
github-action-benchmark <github@users.noreply.github.com>
2024-09-13 11:00:12 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7e61a85e6cdab572fe2b517fe7045781194807a4

--- premium/time/list/data.js
+++ premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725772432480,
+  "lastUpdate": 1726225212433,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -65,6 +65,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "308.92000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "98318303+souravgupta-msft@users.noreply.github.com",
+            "name": "Sourav Gupta",
+            "username": "souravgupta-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7e61a85e6cdab572fe2b517fe7045781194807a4",
+          "message": "Updating codeowners (#1520)",
+          "timestamp": "2024-09-13T15:20:17+05:30",
+          "tree_id": "0159fe8edf54260d8f761a1e5227cc318060d19f",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/7e61a85e6cdab572fe2b517fe7045781194807a4"
+        },
+        "date": 1726225212207,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "10.91000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "323.49000",
             "unit": "seconds"
           }
         ]
