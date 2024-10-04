4282d3c1200497ebf67242fca9022e3dcf7de6f2
Anubhuti Shruti <ashruti@microsoft.com>
2024-10-04 15:51:08 +0530

    add

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 ./blobfuse2 unmount all
+echo "Mount blobfuse and create 10GB file"
 ./blobfuse2 ~/mntdir && dd if=/dev/urandom of=~/mntdir/new10 bs=1M count=10000
 echo "--------------------------------------------------------------------------------"
 echo "File created in mntdir"
 echo "--------------------------------------------------------------------------------"
 ./blobfuse2 unmount all
+echo "Mount and Run FIO test"
 ./blobfuse2 ~/mntdir && fio fio_temp.cfg
 echo "--------------------------------------------------------------------------------"
 echo "FIO test completed"
