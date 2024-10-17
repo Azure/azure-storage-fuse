c763fed6e8a406b67b1b00ba57cfe524a138c0c0
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 11:08:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 50cdc3b81b7a025a35f154c8367f1827ac75adad

--- premium/time/list/data.js
+++ premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728990177584,
+  "lastUpdate": 1729163294941,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -429,6 +429,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "349.38000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "64532198+vibhansa-msft@users.noreply.github.com",
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "50cdc3b81b7a025a35f154c8367f1827ac75adad",
+          "message": "Update CODEOWNERS",
+          "timestamp": "2024-10-17T15:23:37+05:30",
+          "tree_id": "25ffa0338213439f79e5915238c0e52e506b1b45",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/50cdc3b81b7a025a35f154c8367f1827ac75adad"
+        },
+        "date": 1729163294684,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "12.39000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "346.99000",
             "unit": "seconds"
           }
         ]
