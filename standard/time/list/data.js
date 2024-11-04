85faa471da2b0b694e1e8878fbfb8364a4241ef7
github-action-benchmark <github@users.noreply.github.com>
2024-11-04 16:04:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ba739b620646363423170970fd7781339adcdca9

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729000842952,
+  "lastUpdate": 1730736280771,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -365,6 +365,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "473.88000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "syeleti@microsoft.com",
+            "name": "syeleti-msft",
+            "username": "syeleti-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ba739b620646363423170970fd7781339adcdca9",
+          "message": "Fix ReadPanic (#1533)\n\n* Fix Read Panic",
+          "timestamp": "2024-11-04T18:50:08+05:30",
+          "tree_id": "b57a82f3443eb38667887cb22ec37da11d54617a",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/ba739b620646363423170970fd7781339adcdca9"
+        },
+        "date": 1730736280476,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "9.04000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "518.88000",
             "unit": "seconds"
           }
         ]
