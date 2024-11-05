058d4a3e66b2c657e32b53a3ff44b3b791fa03ca
github-action-benchmark <github@users.noreply.github.com>
2024-11-05 11:31:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0fb27e40864763f2c379134fb6e65bab9ebe4cd3

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730796333314,
+  "lastUpdate": 1730806318946,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -433,6 +433,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "465.55000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "31076415+abhiguptacse@users.noreply.github.com",
+            "name": "Abhinav Gupta",
+            "username": "abhiguptacse"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0fb27e40864763f2c379134fb6e65bab9ebe4cd3",
+          "message": "Adding custom component feature (#1558)\n\n* adding custom component feature",
+          "timestamp": "2024-11-05T13:10:41+05:30",
+          "tree_id": "8ea42764d92489abc1d9e99857af7505ea29622e",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/0fb27e40864763f2c379134fb6e65bab9ebe4cd3"
+        },
+        "date": 1730806318629,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "11.17000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "466.98000",
             "unit": "seconds"
           }
         ]
