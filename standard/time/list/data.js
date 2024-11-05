f36395566f149c30517a6d60eb50d9f3cb99103a
github-action-benchmark <github@users.noreply.github.com>
2024-11-05 08:45:33 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 46a557ce18bc597d06f560f73f4d792c70a72563

--- standard/time/list/data.js
+++ standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730736280771,
+  "lastUpdate": 1730796333314,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -399,6 +399,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "518.88000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "137055338+ashruti-msft@users.noreply.github.com",
+            "name": "ashruti-msft",
+            "username": "ashruti-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "46a557ce18bc597d06f560f73f4d792c70a72563",
+          "message": "Redirecting stream config to BlockCache (#1445)\n\n* Auto convert streaming config to block-cache config",
+          "timestamp": "2024-11-05T11:38:27+05:30",
+          "tree_id": "73c016b08ff97d4da1a648b55454b4e182c84916",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/46a557ce18bc597d06f560f73f4d792c70a72563"
+        },
+        "date": 1730796333022,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "11.32000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "465.55000",
             "unit": "seconds"
           }
         ]
