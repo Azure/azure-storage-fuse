e7c3943790219fb6a9d48639eba764dbbddb3add
pragyagandhi <pragyagandhi22@gmail.com>
2024-12-23 14:25:38 +0000

    Updated

--- nfs/build.sh
+++ nfs/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+export VCPKG_ROOT=/home/pragya/azure-storage-fuse/nfs/extern/vcpkg
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF ..
+#cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF ..
 #cmake -DCMAKE_BUILD_TYPE=Release ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_NO_FUSE=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TCMALLOC=OFF -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake ..
+cmake --build .
 make
