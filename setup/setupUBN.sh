992659822b17cab4e1208a0c1cb5a92c735c78fa
Anubhuti Shruti <ashruti@microsoft.com>
2025-01-16 17:07:01 +0530

    Added steps for blobfuse2 installation

--- setup/setupUBN.sh
+++ setup/setupUBN.sh
@@ -21,7 +21,25 @@ echo "Installed all dependencies" -y
 sudo sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
 echo "Uncommented user_allow_other in /etc/fuse.conf"
 
+# Add Microsoft Linux repository for Ubuntu
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list)"
+sudo apt update
+
+# Install Blobfuse2
+sudo apt install blobfuse2 -y
+echo "Installed Blobfuse2"
+
+#Blobfuse2 version
+blobfuse2 --version
+
+#Build blobfuse2 from repo
+#Navigate to the parent directory of the project and run
+#./build.sh
+
 # For not entering password every time on running sudo command, add this line at the end of the 
 # /etc/sudoers file,
 # <user_name> ALL=(ALL:ALL) NOPASSWD:ALL
+
+
 
