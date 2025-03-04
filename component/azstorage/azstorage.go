ca919eb434f97cdb17982ff0b2e891e8e4a811f1
Srinivas Yeleti <syeleti@microsoft.com>
2025-03-04 08:07:32 +0000

    modify Ouput Strings

--- component/azstorage/azstorage.go
+++ component/azstorage/azstorage.go
@@ -159,7 +159,7 @@ func (az *AzStorage) configureAndTest(isParent bool) error {
 		err = az.storage.TestPipeline()
 		if err != nil {
 			log.Err("AzStorage::configureAndTest : Failed to validate credentials [%s]", err.Error())
-			return fmt.Errorf("Failed to Authenticate %s Credentials with Error [%s]", az.Name(), err.Error())
+			return fmt.Errorf("failed to authenticate %s credentials with error [%s]", az.Name(), err.Error())
 		}
 	}
 
