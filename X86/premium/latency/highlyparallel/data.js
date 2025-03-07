c5c75fc964774faa288dc3cf0be060422b9fedec
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 13:20:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for db543abdaf167da96dc1aab0033b0b26c065bf7c

--- X86/premium/latency/highlyparallel/data.js
+++ X86/premium/latency/highlyparallel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741349493633,
+  "lastUpdate": 1741353608742,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "rand_read_128_thread",
             "value": 1812.9109512858774,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "db543abdaf167da96dc1aab0033b0b26c065bf7c",
+          "message": "Added step to cleanup block-cache temp path on start",
+          "timestamp": "2025-03-07T04:43:37-08:00",
+          "tree_id": "8efca96f31bbb941ccd6e7c17a880599f40282f3",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/db543abdaf167da96dc1aab0033b0b26c065bf7c"
+        },
+        "date": 1741353608502,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "seq_write_112_thread",
+            "value": 298.38011198302496,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "seq_read_128_thread",
+            "value": 333.92896987345466,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "rand_read_128_thread",
+            "value": 1885.8875014600133,
             "unit": "milliseconds"
           }
         ]
