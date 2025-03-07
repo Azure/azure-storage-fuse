a5167106a52d862c1101a4e183d840d4f9e01d9b
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 13:20:07 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for db543abdaf167da96dc1aab0033b0b26c065bf7c

--- X86/premium/bandwidth/highlyparallel/data.js
+++ X86/premium/bandwidth/highlyparallel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741349492383,
+  "lastUpdate": 1741353607670,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "rand_read_128_thread",
             "value": 4319.4111328125,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "db543abdaf167da96dc1aab0033b0b26c065bf7c",
+          "message": "Added step to cleanup block-cache temp path on start",
+          "timestamp": "2025-03-07T04:43:37-08:00",
+          "tree_id": "8efca96f31bbb941ccd6e7c17a880599f40282f3",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/db543abdaf167da96dc1aab0033b0b26c065bf7c"
+        },
+        "date": 1741353607404,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "seq_write_112_thread",
+            "value": 24226.3388671875,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "seq_read_128_thread",
+            "value": 24947.865559895832,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "rand_read_128_thread",
+            "value": 4149.950846354167,
             "unit": "MiB/s"
           }
         ]
