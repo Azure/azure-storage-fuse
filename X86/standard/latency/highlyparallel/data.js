e3cec6cc466b306d5da146954d2ef83c0d9dc0dd
github-action-benchmark <github@users.noreply.github.com>
2025-03-08 09:49:33 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 03e72e47d37985e5c28051c0ff17bdc0c7315e74

---
+++ X86/standard/latency/highlyparallel/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741427373095,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "03e72e47d37985e5c28051c0ff17bdc0c7315e74",
+          "message": "Correcting code for cache cleanup",
+          "timestamp": "2025-03-07T23:48:22-08:00",
+          "tree_id": "5b69276c81c0c728ae2dd3889b9743194fdcc990",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/03e72e47d37985e5c28051c0ff17bdc0c7315e74"
+        },
+        "date": 1741427372826,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "seq_write_112_thread",
+            "value": 299.83574722160796,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "seq_read_128_thread",
+            "value": 299.78584744104137,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "rand_read_128_thread",
+            "value": 1700.7222613639708,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
