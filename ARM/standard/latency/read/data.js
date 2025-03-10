de5b2efcab93949f5607e7a1c008b6e67aac9488
github-action-benchmark <github@users.noreply.github.com>
2025-03-10 02:39:21 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 03e72e47d37985e5c28051c0ff17bdc0c7315e74

--- ARM/standard/latency/read/data.js
+++ ARM/standard/latency/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741440596904,
+  "lastUpdate": 1741574361064,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -68,6 +68,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "random_read_4_threads",
             "value": 126.16549849148068,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "vibhansa@microsoft.com",
+            "name": "vibhansa",
+            "username": "vibhansa-msft"
+          },
+          "distinct": true,
+          "id": "03e72e47d37985e5c28051c0ff17bdc0c7315e74",
+          "message": "Correcting code for cache cleanup",
+          "timestamp": "2025-03-07T23:48:22-08:00",
+          "tree_id": "5b69276c81c0c728ae2dd3889b9743194fdcc990",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/03e72e47d37985e5c28051c0ff17bdc0c7315e74"
+        },
+        "date": 1741574360820,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read",
+            "value": 0.112830981874,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read",
+            "value": 161.12483343482901,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_small_file",
+            "value": 0.080684212433,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 0.16120097734500002,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_direct_io",
+            "value": 0.11163954469699999,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_direct_io",
+            "value": 157.97103734915567,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_4_threads",
+            "value": 0.13381242667066665,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_16_threads",
+            "value": 0.5606234703996665,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_4_threads",
+            "value": 161.78902809482898,
             "unit": "milliseconds"
           }
         ]
