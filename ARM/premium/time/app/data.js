8032abf7bb6332b8598638b9a2329a0d1b73d3e1
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 07:11:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0cd7596da267f6a7f6ad7cd9126fc9ae1305d3bd

--- ARM/premium/time/app/data.js
+++ ARM/premium/time/app/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744303017581,
+  "lastUpdate": 1744355513755,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -255,6 +255,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "read_40GB",
             "value": 13.353879451751709,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft",
+            "email": "64532198+vibhansa-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0cd7596da267f6a7f6ad7cd9126fc9ae1305d3bd",
+          "message": "Adding arm based benchmark tests (#1654)\n\nCo-authored-by: Srinivas Yeleti <syeleti@microsoft.com>",
+          "timestamp": "2025-04-10T15:43:25Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/0cd7596da267f6a7f6ad7cd9126fc9ae1305d3bd"
+        },
+        "date": 1744355513529,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "write_1GB",
+            "value": 0.8896656036376953,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_10GB",
+            "value": 4.077868700027466,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_100GB",
+            "value": 37.574944496154785,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_40GB",
+            "value": 15.700565338134766,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_1GB",
+            "value": 0.732691764831543,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_10GB",
+            "value": 3.349646806716919,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_100GB",
+            "value": 32.546515464782715,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_40GB",
+            "value": 12.951375484466553,
             "unit": "seconds"
           }
         ]
