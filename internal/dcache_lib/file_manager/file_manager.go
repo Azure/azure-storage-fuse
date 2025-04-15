fc868683bcc1a013fc5b6a1de4fc8820bf4011ef
Srinivas Yeleti <syeleti@microsoft.com>
2025-04-15 05:35:38 +0000

    Modify FileLayout Interface

--- internal/dcache_lib/file_manager/file_manager.go
+++ internal/dcache_lib/file_manager/file_manager.go
@@ -33,20 +33,20 @@
 
 package file_manager
 
-type fileLayoutMgr interface {
+type IFileLayoutMgr interface {
 	// Create Placeholder file to prevent the creation from the other nodes
-	CreateNewFileLayout(fileName string) *fileLayout
+	CreateNewFileLayout(fileName string) (*FileLayout, error)
 	// Update the fileSize and its state and update the .md file.
 	// The Call comes when the close call comes to write FD.
-	ConfirmFileLayout(file *fileLayout, size int64) error
+	ConfirmFileLayout(file *FileLayout, size int64) error
 	// Delete Filelayout incase of write/close failure.(remove .md file)
-	DeleteFileLayout(file *fileLayout) error
+	DeleteFileLayout(file *FileLayout) error
 	// Check File is present in Dcache
-	CheckFileInDCache(fileName string) (isPresent bool, f *fileLayout)
+	CheckFileInDCache(fileName string) (isPresent bool, f *FileLayout)
 	// Check File is present in Azure.
 	CheckFileInAzure(fileName string) (isPresent bool)
 	// Increments File read FD count in .md file.
-	IncrementFDCount(file *fileLayout) error
+	IncrementFDCount(file *FileLayout) error
 	// Decrements File read FD count in .md file.
-	DecrementFDCount(file *fileLayout) error
+	DecrementFDCount(file *FileLayout) error
 }
--- internal/dcache_lib/file_manager/layout.go
+++ internal/dcache_lib/file_manager/layout.go
@@ -43,51 +43,58 @@ const (
 	Deleting
 )
 
-type fileLayout struct {
-	fileName        string
-	fileId          string // UUID to represent the file inside Dcache
-	size            int64
-	state           fileState
-	openCount       int      // Number of Read Fd's present for this file accross the dCache
-	clustermapEpoch int      // Clustermap Epoch value when the file was created.
-	hash            string   // Hash of the entire file data.
-	mvlist          []string // todo: this should be replaced with type of mv.
+type FileLayout struct {
+	FileName        string    `json:"fileName,omitempty"`
+	FileId          string    `json:"fileId,omitempty"`
+	Size            int64     `json:"size,omitempty"`
+	State           fileState `json:"state,omitempty"`
+	OpenCount       int       `json:"open-count,omitempty"`
+	ClustermapEpoch int       `json:"clustermap-epoch,omitempty"`
+	Hash            string    `json:"sha1hash,omitempty"`
+	ChunkSize       int       `json:"chunk-size,omitempty"`
+	StripeSize      int       `json:"stripe-size,omitempty"`
+	Mvlist          []string  `json:"mv-list,omitempty"`
 }
 
-var _ fileLayoutMgr = &fileLayout{}
+var _ IFileLayoutMgr = &FileLayoutMgr{}
 
-// Create Placeholder file to prevent the creation from the other nodes
-func (*fileLayout) CreateNewFileLayout(fileName string) *fileLayout {
-	return nil
+type FileLayoutMgr struct{}
+
+// Check file in Dcache, fail the call if it's already present.
+// Also check the file in Azure, If necessary
+// Choose appropriate mv's
+// Create the place holder .md file in Azure to prevent other nodes to write to same file.
+func (FileLayoutMgr) CreateNewFileLayout(fileName string) (*FileLayout, error) {
+	return nil, nil
 }
 
 // Update the fileSize and its state and update the .md file.
 // The Call comes when the close call comes to write FD.
-func (*fileLayout) ConfirmFileLayout(file *fileLayout, size int64) error {
+func (FileLayoutMgr) ConfirmFileLayout(file *FileLayout, size int64) error {
 	return nil
 }
 
-// Delete Filelayout incase of write/close failure.(remove .md file)
-func (*fileLayout) DeleteFileLayout(file *fileLayout) error {
+// Delete Filelayout incase of write/close failure for writeFD.(remove .md file)
+func (FileLayoutMgr) DeleteFileLayout(file *FileLayout) error {
 	return nil
 }
 
-// Check File is present in Dcache, return filelayout if it's present
-func (*fileLayout) CheckFileInDCache(fileName string) (isPresent bool, f *fileLayout) {
+// Check File is present in Dcache, return filelayout if it's already present
+func (FileLayoutMgr) CheckFileInDCache(fileName string) (isPresent bool, f *FileLayout) {
 	return false, nil
 }
 
-// Check File is present in Azure.
-func (*fileLayout) CheckFileInAzure(fileName string) (isPresent bool) {
+// Check If File is present in Azure.
+func (FileLayoutMgr) CheckFileInAzure(fileName string) (isPresent bool) {
 	return false
 }
 
 // Increments File read FD count in .md file.
-func (*fileLayout) IncrementFDCount(file *fileLayout) error {
+func (FileLayoutMgr) IncrementFDCount(file *FileLayout) error {
 	return errors.New("todo: Implement with unlink/remove")
 }
 
 // Decrements File read FD count in .md file.
-func (*fileLayout) DecrementFDCount(file *fileLayout) error {
+func (FileLayoutMgr) DecrementFDCount(file *FileLayout) error {
 	return errors.New("todo: Implement with unlink/remove")
 }
