262456509e37009d30a1610d38b93fbced942410
github-action-benchmark <github@users.noreply.github.com>
2025-04-20 08:49:19 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 07c91329ece3d6310f1a56cdead7b10e449fc66f

--- ARM/premium/time/list/data.js
+++ ARM/premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744630980305,
+  "lastUpdate": 1745138959520,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -233,6 +233,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "528.95000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft",
+            "email": "64532198+vibhansa-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "07c91329ece3d6310f1a56cdead7b10e449fc66f",
+          "message": "Preload feature drop to main branch (#1678)\n\nCo-authored-by: Sourav Gupta <98318303+souravgupta-msft@users.noreply.github.com>\nCo-authored-by: souravgupta <souravgupta@microsoft.com>",
+          "timestamp": "2025-04-11T09:17:56Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/07c91329ece3d6310f1a56cdead7b10e449fc66f"
+        },
+        "date": 1745138959279,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "12.85000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "519.44000",
             "unit": "seconds"
           }
         ]
