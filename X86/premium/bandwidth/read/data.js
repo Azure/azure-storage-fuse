24d795d39d0508305da79db11a1cf871fd082dba
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 07:51:06 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 43f48e9b789a9fc27d2138c4679ef8dc47cd55bf

--- X86/premium/bandwidth/read/data.js
+++ X86/premium/bandwidth/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745123101291,
+  "lastUpdate": 1745308266273,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -1097,6 +1097,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "random_read_4_threads",
             "value": 12.783854166666666,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "64532198+vibhansa-msft@users.noreply.github.com",
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "43f48e9b789a9fc27d2138c4679ef8dc47cd55bf",
+          "message": "Updating README for preload (#1685)\n\nCo-authored-by: Sourav Gupta <98318303+souravgupta-msft@users.noreply.github.com>",
+          "timestamp": "2025-04-22T12:59:00+05:30",
+          "tree_id": "97239a1303fad55d8f7adfa85b21e4ff6c56579d",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/43f48e9b789a9fc27d2138c4679ef8dc47cd55bf"
+        },
+        "date": 1745308264872,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read",
+            "value": 1102.9729817708333,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read",
+            "value": 3.1575520833333335,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_small_file",
+            "value": 2436.8323567708335,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 1328.1162109375,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_direct_io",
+            "value": 1465.5166015625,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_direct_io",
+            "value": 3.0970052083333335,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_4_threads",
+            "value": 3935.5491536458335,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_16_threads",
+            "value": 2942.9563802083335,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_4_threads",
+            "value": 12.277994791666666,
             "unit": "MiB/s"
           }
         ]
