447f855dcf2ebc73e40239927c4b2c58047b5d92
Akanksha Jain <jainakanksha@microsoft.com>
2025-04-30 16:27:52 +0530

    removed commented code

--- component/distributed_cache/utils.go
+++ component/distributed_cache/utils.go
@@ -52,7 +52,7 @@ func getBlockDeviceUUId(path string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
+	// Call: blkid -o value -s UUID  <path>
 	out, err := exec.Command("blkid", "-o", "value", "-s", "UUID", device).Output()
 	if err != nil {
 		return "", fmt.Errorf("error running blkid: %v", err)
@@ -68,24 +68,13 @@ func getBlockDeviceUUId(path string) (string, error) {
 }
 
 func findMountDevice(path string) (string, error) {
-	// Call: df --output=source <path>
-	// out, err := exec.Command("df", "--output=source", path).Output()
 	// Call: findmnt -n -o SOURCE --target <path>
 	out, err := exec.Command(
 		"findmnt", "-n", "-o", "SOURCE", "--target", path,
 	).Output()
 	if err != nil {
-		// return "", fmt.Errorf("failed to run df on %s: %v", path, err)
 		return "", fmt.Errorf("failed to run findmnt on %s: %v", path, err)
 	}
-	// // df prints a header line, then the device
-	// dfOutString := string(out)
-	// lines := strings.Split(strings.TrimSpace(dfOutString), "\n")
-	// common.Assert(len(lines) == 2, fmt.Sprintf("df output for mount device must return 2 lines %s", out))
-	// if len(lines) != 2 {
-	// 	return "", fmt.Errorf("unexpected df output for %s: %q", path, out)
-	// }
-	// device := strings.TrimSpace(lines[1])
 	device := strings.TrimSpace(string(out))
 	if device == "" {
 		return "", fmt.Errorf("no device found in df output for %s", path)
