d485472d78e47075653e7a8e3a352aea03ccb966
Ubuntu <dcacheuser@dcacheVM-1.gzdhckhh31pe1kewpcporrywue.gx.internal.cloudapp.net>
2025-05-02 16:19:52 +0530

    Added todos and notes

--- internal/dcache/cluster_manager/cluster_manager_impl.go
+++ internal/dcache/cluster_manager/cluster_manager_impl.go
@@ -450,6 +450,9 @@ func (cmi *ClusterManagerImpl) updateStorageClusterMapIfRequired() {
 	}
 }
 
+// TODO :: Empty cachedir on startup
+// TODO :: Handle the case when joinMV fails
+// Remember to give permissions to cachedir otherwise joinMv will fail
 func (cmi *ClusterManagerImpl) updateMVList(rvMap map[string]dcache.RawVolume, existingMVMap map[string]dcache.MirroredVolume, NumReplicas int, MvsPerRv int) (map[string]dcache.MirroredVolume, error) {
 
 	//
