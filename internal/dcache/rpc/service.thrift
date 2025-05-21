c87fd8c13f79ad1b75c3e03813a8e363d9dac321
Sourav Gupta <98318303+souravgupta-msft@users.noreply.github.com>
2025-05-21 09:24:06 +0530

    RPC: Custom RPC response error (#1768)

--- internal/dcache/rpc/service.thrift
+++ internal/dcache/rpc/service.thrift
@@ -5,28 +5,28 @@ include "models.thrift"
 // Define the service with RPC methods
 service ChunkService {
     // check if the node is reachable
-    models.HelloResponse Hello(1: models.HelloRequest request)
+    models.HelloResponse Hello(1: models.HelloRequest request) throws (1:models.ResponseError err)
 
     // fetch the chunk from the node from the given rvID
-    models.GetChunkResponse GetChunk(1: models.GetChunkRequest request)
+    models.GetChunkResponse GetChunk(1: models.GetChunkRequest request) throws (1:models.ResponseError err)
 
     // store the chunk on the node on the given rvID
-    models.PutChunkResponse PutChunk(1: models.PutChunkRequest request)
+    models.PutChunkResponse PutChunk(1: models.PutChunkRequest request) throws (1:models.ResponseError err)
 
     // delete the chunk from the node from the given rvID
-    models.RemoveChunkResponse RemoveChunk(1: models.RemoveChunkRequest request)
+    models.RemoveChunkResponse RemoveChunk(1: models.RemoveChunkRequest request) throws (1:models.ResponseError err)
 
     // add RV to the given MV
-    models.JoinMVResponse JoinMV(1: models.JoinMVRequest request)
+    models.JoinMVResponse JoinMV(1: models.JoinMVRequest request) throws (1:models.ResponseError err)
 
     // update the component RVs for the given MV
     // this call is sent after the JoinMV call to the online RVs to update their component RVs list
-    models.UpdateMVResponse UpdateMV(1: models.UpdateMVRequest request)
+    models.UpdateMVResponse UpdateMV(1: models.UpdateMVRequest request) throws (1:models.ResponseError err)
 
     // remove RV from the given MV
-    models.LeaveMVResponse LeaveMV(1: models.LeaveMVRequest request)
+    models.LeaveMVResponse LeaveMV(1: models.LeaveMVRequest request) throws (1:models.ResponseError err)
 
-    models.StartSyncResponse StartSync(1: models.StartSyncRequest request)
+    models.StartSyncResponse StartSync(1: models.StartSyncRequest request) throws (1:models.ResponseError err)
 
-    models.EndSyncResponse EndSync(1: models.EndSyncRequest request)
+    models.EndSyncResponse EndSync(1: models.EndSyncRequest request) throws (1:models.ResponseError err)
 }
