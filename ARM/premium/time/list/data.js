a5353dee57eb2d75851468ee2501c17d716eef79
github-action-benchmark <github@users.noreply.github.com>
2025-05-25 08:58:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5e0ecbfd0e63a70dca732601790538e455816b41

--- ARM/premium/time/list/data.js
+++ ARM/premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747558374646,
+  "lastUpdate": 1748163531145,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -393,6 +393,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": "486.76000",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ashruti-msft",
+            "username": "ashruti-msft",
+            "email": "137055338+ashruti-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "5e0ecbfd0e63a70dca732601790538e455816b41",
+          "message": "Mariner preview release location fix (#1785)",
+          "timestamp": "2025-05-23T09:32:31Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/5e0ecbfd0e63a70dca732601790538e455816b41"
+        },
+        "date": 1748163530851,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": "11.02000",
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": "484.27000",
             "unit": "seconds"
           }
         ]
