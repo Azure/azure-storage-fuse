ef25c7462b0b8d5cd42edbf6916fdd87c666d3f7
Akanksha Jain <jainakanksha@microsoft.com>
2025-05-29 14:43:21 +0530

    Updated the leaderNode evaluation

--- internal/dcache/cluster_manager/cluster_manager.go
+++ internal/dcache/cluster_manager/cluster_manager.go
@@ -1212,26 +1212,6 @@ func (cmi *ClusterManager) updateStorageClusterMapIfRequired() error {
 		return err
 	}
 
-	clusterMapAge := now - clusterMap.LastUpdatedAt
-	//
-	// Assert if clusterMap is not updated for 3 consecutive epochs, it might indicate some bug.
-	// For very small ClustermapEpoch values, 3 times the value will not be sufficient as the
-	// thresholdClusterMapEpochTime is set to 60, so limit it to 180.
-	// The max time till which the clusterMap may not be updated in the event of leader going down is
-	// 2*ClustermapEpoch + thresholdClusterMapEpochTime, so for values of ClustermapEpoch above 60 seconds, 3 times
-	// ClustermapEpoch is suffcient but for smaller ClustermapEpoch values we have to cap to 180, with a margin
-	// of 20 seconds.
-	//
-	common.Assert(clusterMapAge < int64(max(clusterMap.Config.ClustermapEpoch*3, 200)),
-		fmt.Sprintf("clusterMapAge (%d) >= %d",
-			clusterMapAge, int64(max(clusterMap.Config.ClustermapEpoch*3, 200))))
-
-	// Staleness check for non-leader.
-	stale := clusterMapAge > int64(clusterMap.Config.ClustermapEpoch+thresholdClusterMapEpochTime)
-	// Are we the leader node? Leader gets to update the clustermap bypassing the staleness check.
-	leaderNode := clusterMap.LastUpdatedBy
-	leader := (leaderNode == cmi.myNodeId)
-
 	//
 	// If some other node/context is currently updating the clustermap, skip updating in this iteration, as
 	// long as the staleness threshold is not met.
@@ -1248,7 +1228,7 @@ func (cmi *ClusterManager) updateStorageClusterMapIfRequired() error {
 
 	if isClusterMapUpdateBlocked {
 		log.Debug("ClusterManager::updateStorageClusterMapIfRequired:skipping, clustermap is being updated by (leader %s), current node (%s)",
-			leaderNode, cmi.myNodeId)
+			clusterMap.LastUpdatedBy, cmi.myNodeId)
 
 		//
 		// Leader node should have updated the state to checking and it should not find the state to checking.
@@ -1261,9 +1241,31 @@ func (cmi *ClusterManager) updateStorageClusterMapIfRequired() error {
 		//       finds the clusterMap state as checking.
 		//       Still leaving the assert as it's useful to see if it occurrs in any other way.
 		//
-		common.Assert(!leader, "We don't expect leader to see the clustermap in checking state")
+		common.Assert(!(clusterMap.LastUpdatedBy == cmi.myNodeId), "We don't expect leader to see the clustermap in checking state")
 		return nil
 	}
+
+	clusterMapAge := now - clusterMap.LastUpdatedAt
+	//
+	// Assert if clusterMap is not updated for 3 consecutive epochs, it might indicate some bug.
+	// For very small ClustermapEpoch values, 3 times the value will not be sufficient as the
+	// thresholdClusterMapEpochTime is set to 60, so limit it to 180.
+	// The max time till which the clusterMap may not be updated in the event of leader going down is
+	// 2*ClustermapEpoch + thresholdClusterMapEpochTime, so for values of ClustermapEpoch above 60 seconds, 3 times
+	// ClustermapEpoch is suffcient but for smaller ClustermapEpoch values we have to cap to 180, with a margin
+	// of 20 seconds.
+	//
+	common.Assert(clusterMapAge < int64(max(clusterMap.Config.ClustermapEpoch*3, 200)),
+		fmt.Sprintf("clusterMapAge (%d) >= %d",
+			clusterMapAge, int64(max(clusterMap.Config.ClustermapEpoch*3, 200))))
+
+	// Staleness check for non-leader.
+	stale := clusterMapAge > int64(clusterMap.Config.ClustermapEpoch+thresholdClusterMapEpochTime)
+
+	// Are we the leader node? Leader gets to update the clustermap bypassing the staleness check.
+	// Since clusterMapBeingUpdatedByAnotherNode may override LastUpdatedBy, re-evaluate leaderNode afterward
+	leaderNode := clusterMap.LastUpdatedBy
+	leader := (leaderNode == cmi.myNodeId)
 
 	//
 	// Ok, clustermap can be possibly updated (can't be sure until startClusterMapUpdate() returns success).
