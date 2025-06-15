021dc82a1503e09e649efa61633b02af03a49b75
github-action-benchmark <github@users.noreply.github.com>
2025-06-15 10:49:57 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5e0ecbfd0e63a70dca732601790538e455816b41

--- ARM/standard/bandwidth/app/data.js
+++ ARM/standard/bandwidth/app/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749379814229,
+  "lastUpdate": 1749984597514,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -875,6 +875,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "read_40GB",
             "value": 3160.3963841182695,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ashruti-msft",
+            "username": "ashruti-msft",
+            "email": "137055338+ashruti-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "5e0ecbfd0e63a70dca732601790538e455816b41",
+          "message": "Mariner preview release location fix (#1785)",
+          "timestamp": "2025-05-23T09:32:31Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/5e0ecbfd0e63a70dca732601790538e455816b41"
+        },
+        "date": 1749984597208,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "write_1GB",
+            "value": 487.5036198524758,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "write_10GB",
+            "value": 1757.2931651784286,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "write_100GB",
+            "value": 2732.623575011832,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "write_40GB",
+            "value": 2725.5732320769334,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "read_1GB",
+            "value": 1177.2761222892323,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "read_10GB",
+            "value": 2798.3473016888624,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "read_100GB",
+            "value": 3164.5983406369987,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "read_40GB",
+            "value": 3098.846766744721,
             "unit": "MiB/s"
           }
         ]
