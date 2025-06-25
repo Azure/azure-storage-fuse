04523d0bbcfdbf5fbf271012ab03732755955d92
Akanksha Jain <jainakanksha@microsoft.com>
2025-06-25 21:55:37 +0530

    Added degraded workflow

--- test/distributed_cache/test-cluster.sh
+++ test/distributed_cache/test-cluster.sh
@@ -659,22 +659,6 @@ test_cross_node_consistency()
     local large_file_test=${4:-false}
     
     if [ "$large_file_test" == "true" ]; then
-        if [ -n "$dcache_4GB_md5sum" ]; then
-            becho -n "Cross-node large file consistency check for 4GB.dcache between $node1 and $node2"
-            dcache_file_md5_node1=$(get_md5sum $node1 "4GB.dcache" dcache)
-            dcache_file_md5_node2=$(get_md5sum $node2 "4GB.dcache" dcache)
-            [ "$dcache_file_md5_node1" == "$dcache_file_md5_node2" ]
-            log_status $? "$node1: $dcache_file_md5_node1, $node2: $dcache_file_md5_node2"
-        fi
-        
-        if [ -n "$both_4GB_md5sum" ]; then
-            becho -n "Cross-node large file consistency check for 4GB.both between $node1 and $node2"
-            both_file_md5_node1=$(get_md5sum $node1 "4GB.both")
-            both_file_md5_node2=$(get_md5sum $node2 "4GB.both")
-            [ "$both_file_md5_node1" == "$both_file_md5_node2" ]
-            log_status $? "$node1: $both_file_md5_node1, $node2: $both_file_md5_node2"
-        fi
-        
         if [ -n "$dcache_2GB_md5sum" ]; then
             becho -n "Cross-node large file consistency check for 2GB.dcache between $node1 and $node2"
             dcache_file_md5_node1=$(get_md5sum $node1 "2GB.dcache" dcache)
@@ -687,6 +671,22 @@ test_cross_node_consistency()
             becho -n "Cross-node large file consistency check for 2GB.both between $node1 and $node2"
             both_file_md5_node1=$(get_md5sum $node1 "2GB.both")
             both_file_md5_node2=$(get_md5sum $node2 "2GB.both")
+            [ "$both_file_md5_node1" == "$both_file_md5_node2" ]
+            log_status $? "$node1: $both_file_md5_node1, $node2: $both_file_md5_node2"
+        fi
+        
+        if [ -n "$dcache_1GB_md5sum" ]; then
+            becho -n "Cross-node large file consistency check for 1GB.dcache between $node1 and $node2"
+            dcache_file_md5_node1=$(get_md5sum $node1 "1GB.dcache" dcache)
+            dcache_file_md5_node2=$(get_md5sum $node2 "1GB.dcache" dcache)
+            [ "$dcache_file_md5_node1" == "$dcache_file_md5_node2" ]
+            log_status $? "$node1: $dcache_file_md5_node1, $node2: $dcache_file_md5_node2"
+        fi
+        
+        if [ -n "$both_1GB_md5sum" ]; then
+            becho -n "Cross-node large file consistency check for 1GB.both between $node1 and $node2"
+            both_file_md5_node1=$(get_md5sum $node1 "1GB.both")
+            both_file_md5_node2=$(get_md5sum $node2 "1GB.both")
             [ "$both_file_md5_node1" == "$both_file_md5_node2" ]
             log_status $? "$node1: $both_file_md5_node1, $node2: $both_file_md5_node2"
         fi
@@ -846,28 +846,6 @@ for ((current_node=1; current_node<=NUM_NODES; current_node++)); do
     # Test large file operations on specific nodes
     if [ "$cluster_readonly" == "false" ]; then
         if [ $current_node -eq $MIN_NODES ]; then
-            becho -n "Write 4GB data in dcache on $vm_name"
-            file_name="4GB.dcache"
-            write_data_in_dcache $vm_name $file_name 1G 4
-            TOTAL_DCACHE_FILES=$((TOTAL_DCACHE_FILES + 1))
-            TOTAL_BOTH_FILES=$((TOTAL_BOTH_FILES + 1))
-            dcache_4GB_md5sum=$(get_md5sum $vm_name $file_name dcache)
-            log_status $?
-
-            becho -n "Write 4GB data over unqalified path on $vm_name"
-            file_name="4GB.both"
-            write_data_on_both $vm_name $file_name 1G 4
-            TOTAL_BOTH_FILES=$((TOTAL_BOTH_FILES + 1))
-            
-            # When we write file in unqualified path, it writes in azure as well as dcache path. So updating the counter for azure file as well.
-            TOTAL_DCACHE_FILES=$((TOTAL_DCACHE_FILES + 1))
-            TOTAL_AZURE_FILES=$((TOTAL_AZURE_FILES + 1))
-            
-            both_4GB_md5sum=$(get_md5sum $vm_name $file_name)
-            log_status $?
-        fi
-
-        if [ $current_node -eq 5 ]; then
             becho -n "Write 2GB data in dcache on $vm_name"
             file_name="2GB.dcache"
             write_data_in_dcache $vm_name $file_name 1G 2
@@ -879,30 +857,52 @@ for ((current_node=1; current_node<=NUM_NODES; current_node++)); do
             becho -n "Write 2GB data over unqalified path on $vm_name"
             file_name="2GB.both"
             write_data_on_both $vm_name $file_name 1G 2
+            TOTAL_BOTH_FILES=$((TOTAL_BOTH_FILES + 1))
+            
+            # When we write file in unqualified path, it writes in azure as well as dcache path. So updating the counter for azure file as well.
+            TOTAL_DCACHE_FILES=$((TOTAL_DCACHE_FILES + 1))
+            TOTAL_AZURE_FILES=$((TOTAL_AZURE_FILES + 1))
+            
+            both_2GB_md5sum=$(get_md5sum $vm_name $file_name)
+            log_status $?
+        fi
+
+        if [ $current_node -eq 5 ]; then
+            becho -n "Write 1GB data in dcache on $vm_name"
+            file_name="1GB.dcache"
+            write_data_in_dcache $vm_name $file_name 1G 1
+            TOTAL_DCACHE_FILES=$((TOTAL_DCACHE_FILES + 1))
+            TOTAL_BOTH_FILES=$((TOTAL_BOTH_FILES + 1))
+            dcache_1GB_md5sum=$(get_md5sum $vm_name $file_name dcache)
+            log_status $?
+
+            becho -n "Write 1GB data over unqalified path on $vm_name"
+            file_name="1GB.both"
+            write_data_on_both $vm_name $file_name 1G 1
 
             TOTAL_BOTH_FILES=$((TOTAL_BOTH_FILES + 1))
             
             # When we write file in unqualified path, it writes in azure as well as dcache path. So updating the counter for azure file as well.
             TOTAL_DCACHE_FILES=$((TOTAL_DCACHE_FILES + 1))
             TOTAL_AZURE_FILES=$((TOTAL_AZURE_FILES + 1))
-            both_2GB_md5sum=$(get_md5sum $vm_name $file_name)
+            both_1GB_md5sum=$(get_md5sum $vm_name $file_name)
             log_status $?
         
         fi
 
         # Test accessibility of files created on different nodes
-        if [ -n "$dcache_4GB_md5sum" ]; then
-            becho -n "Verify 4GB dcache file is accessible from $vm_name"
-            dcache_4GB_md5sum_current=$(get_md5sum $vm_name "4GB.dcache" dcache)
-            [ "$dcache_4GB_md5sum" == "$dcache_4GB_md5sum_current" ]
-            log_status $? "Original: $dcache_4GB_md5sum, Current: $dcache_4GB_md5sum_current"
+        if [ -n "$dcache_2GB_md5sum" ]; then
+            becho -n "Verify 2GB dcache file is accessible from $vm_name"
+            dcache_2GB_md5sum_current=$(get_md5sum $vm_name "2GB.dcache" dcache)
+            [ "$dcache_2GB_md5sum" == "$dcache_2GB_md5sum_current" ]
+            log_status $? "Original: $dcache_2GB_md5sum, Current: $dcache_2GB_md5sum_current"
         fi
 
-        if [ -n "$both_4GB_md5sum" ]; then
-            becho -n "Verify 4GB both file is accessible from $vm_name"
-            both_4GB_md5sum_current=$(get_md5sum $vm_name "4GB.both")
-            [ "$both_4GB_md5sum" == "$both_4GB_md5sum_current" ]
-            log_status $? "Original: $both_4GB_md5sum, Current: $both_4GB_md5sum_current"
+        if [ -n "$both_2GB_md5sum" ]; then
+            becho -n "Verify 2GB both file is accessible from $vm_name"
+            both_2GB_md5sum_current=$(get_md5sum $vm_name "2GB.both")
+            [ "$both_2GB_md5sum" == "$both_2GB_md5sum_current" ]
+            log_status $? "Original: $both_2GB_md5sum, Current: $both_2GB_md5sum_current"
         fi
     fi
 
@@ -923,14 +923,6 @@ for ((current_node=1; current_node<=NUM_NODES; current_node++)); do
         wbecho ">> Validating file listing consistency across all nodes"
         echo
         test_file_listing_consistency "$CURRENT_NODES" $TOTAL_AZURE_FILES $TOTAL_DCACHE_FILES $TOTAL_BOTH_FILES
-
-        echo
-        wbecho ">> Testing cross-node file consistency"
-        echo
-
-        # Test middle node with current node
-        middle_node=$((current_node / 2))
-        test_cross_node_consistency "vm$middle_node" "$vm_name" $middle_node false
         
         # Test large files if we have the minimum nodes
         if [ $current_node -ge $MIN_NODES ]; then
@@ -945,56 +937,125 @@ for ((current_node=1; current_node<=NUM_NODES; current_node++)); do
         fi
     fi
 
+done  # End of the main loop starting nodes
+
+############################################################################
+##    Test blobfuse Process Failure Over a node Degraded Workflow         ##
+############################################################################
+
+echo
+echo "======================================================================"
+wbecho ">> Testing blobfuse process Failure over a node Degraded Workflow"
+echo "======================================================================"
+echo
+
+# Choose the first node to stop blobfuse - this is typically a more critical node
+failed_node_vm="vm1"
+failed_node_id=$(get_node_id $failed_node_vm)
+
+# We'll use the second node to read clustermap info (since we're taking down vm1)
+monitoring_node="vm2"
+
+# Read current clustermap from the monitoring node to get RVs on the failing node
+becho -n "Reading clustermap before node failure"
+cm_before=$(read_clustermap_from_node $monitoring_node)
+log_status $?
+
+# Get the list of RVs assigned to the node we're about to take down
+becho -n "Identifying RVs assigned to $failed_node_vm"
+rvs_on_failing_node=$(get_rv_list_for_node "$cm_before" "$failed_node_id")
+[ -n "$rvs_on_failing_node" ]
+log_status $? "No RVs found on $failed_node_vm"
+
+# Find MVs that use these RVs
+becho -n "Finding MVs that use RVs on $failed_node_vm"
+mvs_using_rvs=$(get_mvs_count_for_given_rv_with_state "$cm_before" "$rvs_on_failing_node" "online")
+[ "$mvs_using_rvs" -gt 0 ]
+log_status $? "Found $mvs_using_rvs MVs using RVs on $failed_node_vm"
+
+# Take down the blobfuse over the node
+wbecho ">> Taking down blobfuse over node $failed_node_vm to simulate failure"
+stop_blobfuse_on_node $failed_node_vm
+
+# Wait for heartbeat expiry (HB_SECONDS * HB_TILL_NODE_DOWN + CLUSTERMAP_EPOCH+ buffer)
+hb_timeout=$((HB_SECONDS * HB_TILL_NODE_DOWN + $CLUSTERMAP_EPOCH + 5))
+wbecho ">> Waiting $hb_timeout seconds for heartbeat timeout..."
+sleep $hb_timeout
+wbecho ">> Heartbeat timeout period completed"
+
+# Read the clustermap from the monitoring node to verify the node is marked as down
+becho -n "Reading clustermap after node failure"
+cm_after=$(read_clustermap_from_node $monitoring_node)
+log_status $?
+
+# Check if the RVs on the failed node are now marked as offline
+becho -n "Checking if RVs on $failed_node_vm are now offline"
+for rv in $(echo $rvs_on_failing_node | tr ',' ' '); do
+    rv_state=$(get_rv_state "$cm_after" "$rv")
+    [ "$rv_state" != "online" ]
+    if [ $? -ne 0 ]; then
+        log_status 1 "RV $rv is still online after node failure"
+    fi
 done
+log_status 0 "RVs on failed node are now offline"
+
+# Check if MVs that used the RVs on the failed node are now in degraded state
+becho -n "Checking if MVs using RVs on $failed_node_vm are now degraded"
+degraded_mvs=$(get_mv_count_with_state "$cm_after" "degraded")
+[ "$degraded_mvs" -gt 0 ]
+log_status $? "Found $degraded_mvs degraded MVs"
+
+# Create a list of active nodes (excluding the failed node) for Comprehensive validation
+ACTIVE_NODES=""
+for node in $ALL_NODES; do
+    if [ "$node" != "$failed_node_vm" ]; then
+        ACTIVE_NODES="$ACTIVE_NODES $node"
+    fi
+done
+becho "Active nodes for validation: $ACTIVE_NODES"
 
 ############################################################################
-##                    Final Comprehensive Validation                      ##
+##                    Comprehensive Validation                      ##
 ############################################################################
 
+# Comprehensive clustermap consistency validation
 echo
-echo "======================================================================"
-wbecho ">> Final Comprehensive Cluster Validation"
-echo "======================================================================"
+wbecho ">> clustermap consistency validation across active nodes"
 echo
+validate_clustermap_consistency $NUM_NODES "$ACTIVE_NODES"
 
-# Final clustermap consistency validation
-echo
-wbecho ">> clustermap consistency validation across all nodes"
-echo
-validate_clustermap_consistency $NUM_NODES "$ALL_NODES"
-
-# Final RV-node mapping validation
+# Comprehensive RV-node mapping validation
 echo
 wbecho ">> RV-node mapping consistency validation"
 echo
-validate_rv_node_mapping_consistency "$ALL_NODES" $NUM_NODES
+validate_rv_node_mapping_consistency "$ACTIVE_NODES" $NUM_NODES
 
-# Final file listing consistency validation
+# Comprehensive file listing consistency validation
 echo
 wbecho ">> File listing consistency validation"
 echo
-test_file_listing_consistency "$ALL_NODES" $TOTAL_AZURE_FILES $TOTAL_DCACHE_FILES $TOTAL_BOTH_FILES
+test_file_listing_consistency "$ACTIVE_NODES" $TOTAL_AZURE_FILES $TOTAL_DCACHE_FILES $TOTAL_BOTH_FILES
 
 # Comprehensive cross-node file consistency testing
 echo
 wbecho ">> Comprehensive cross-node file consistency testing"
 echo
 
-
-# Test all file types between first and last nodes
-test_cross_node_consistency "vm1" "vm$NUM_NODES" $NUM_NODES false
-
-# Test large file consistency across multiple node pairs
-if [ $NUM_NODES -ge $MIN_NODES ]; then
-    test_cross_node_consistency "vm1" "vm$NUM_NODES" 0 true
+# Use active nodes for cross-node consistency tests
+if [ "$NUM_NODES" -gt 2 ]; then
+    # Find a node that's not the failed node for testing
+    second_node=$(echo $ACTIVE_NODES | awk '{print $1}')
+    last_node=$(echo $ACTIVE_NODES | awk '{print $NF}')
     
-    if [ $NUM_NODES -ge 5 ]; then
-        test_cross_node_consistency "vm$MIN_NODES" "vm5" 0 true
-        test_cross_node_consistency "vm1" "vm5" 0 true
+    test_cross_node_consistency "$second_node" "$last_node" $NUM_NODES false
+    
+    # Test large file consistency across multiple node pairs
+    if [ $NUM_NODES -ge $MIN_NODES ] && [ "$cluster_readonly" == "false" ]; then
+        test_cross_node_consistency "$second_node" "$last_node" 0 true
     fi
 fi
 
 echo
 sbecho "======================================================================"
-sbecho "All cluster validation tests completed successfully!"
+sbecho "Cluster validation tests completed successfully!"
 sbecho "======================================================================"
