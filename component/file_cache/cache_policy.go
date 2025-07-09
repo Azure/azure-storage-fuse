5a8bc6fbcf69900dea7f46a011ca0fb71de4cead
Keith Pimm <keithpimm@microsoft.com>
2025-07-09 13:24:21 -0500

    Fix error message output

--- component/file_cache/cache_policy.go
+++ component/file_cache/cache_policy.go
@@ -82,13 +82,13 @@ func getUsagePercentage(path string, maxSize float64) float64 {
 	if maxSize == 0 {
 		currSize, usagePercent, err = common.GetDiskUsageFromStatfs(path)
 		if err != nil {
-			log.Err("cachePolicy::getUsagePercentage : failed to get disk usage for %s [%v]", path, err.Error)
+			log.Err("cachePolicy::getUsagePercentage : failed to get disk usage for %s [%v]", path, err.Error())
 		}
 	} else {
 		// We need to compuate % usage of temp directory against configured limit
 		currSize, err = common.GetUsageInMegabytes(path)
 		if err != nil {
-			log.Err("cachePolicy::getUsagePercentage : failed to get directory usage for %s [%v]", path, err.Error)
+			log.Err("cachePolicy::getUsagePercentage : failed to get directory usage for %s [%v]", path, err.Error())
 		}
 
 		usagePercent = (currSize / float64(maxSize)) * 100
