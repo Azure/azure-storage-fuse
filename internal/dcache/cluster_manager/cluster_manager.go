36c7e269331b17c3edbcd6ee8e3ebb6f2a8e4ad6
Nagendra Tomar <linuxsmiths@gmail.com>
2025-08-07 13:45:01 +0000

    Self audit

--- internal/dcache/cluster_manager/cluster_manager.go
+++ internal/dcache/cluster_manager/cluster_manager.go
@@ -2374,6 +2374,9 @@ func (cmi *ClusterManager) updateMVList(rvMap map[string]dcache.RawVolume,
 				// Remove the bad RV from MV. Do this before assigning the replacement RV, in case both
 				// are same.
 				//
+				log.Debug("ClusterManager::fixMV: Replacing (%s/%s [%s] -> %s/%s [%s])",
+					rvName, mvName, mv.RVs[rvName], newRvName, mvName, dcache.StateOutOfSync)
+
 				delete(mv.RVs, rvName)
 				mv.RVs[newRvName] = dcache.StateOutOfSync
 
@@ -2382,9 +2385,6 @@ func (cmi *ClusterManager) updateMVList(rvMap map[string]dcache.RawVolume,
 				// We don't yet update existingMVMap, we will do it once joinMV() returns
 				// successfully.
 				//
-
-				log.Debug("ClusterManager::fixMV: Replacing (%s/%s [%s] -> %s/%s)",
-					rvName, mvName, mv.RVs[rvName], newRvName, mvName)
 				foundReplacement = true
 				fixedRVs++
 				break
