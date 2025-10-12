9719ec40f3235cef047d365e72ada31a77c4441c
github-action-benchmark <github@users.noreply.github.com>
2025-10-12 08:35:04 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 389136cf285c96aae19b8e61c3c5bb0cee98bb45

--- X86/standard/time/app/data.js
+++ X86/standard/time/app/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760103194449,
+  "lastUpdate": 1760258104725,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -1871,6 +1871,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "read_40GB",
             "value": 21.46310019493103,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "syeleti-msft",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "389136cf285c96aae19b8e61c3c5bb0cee98bb45",
+          "message": "Fix issues while truncating the file (#2003)\n\nCo-authored-by: Vikas Bhansali <64532198+vibhansa-msft@users.noreply.github.com>\nCo-authored-by: vibhansa <vibhansa@microsoft.com>",
+          "timestamp": "2025-10-10T08:30:20Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/389136cf285c96aae19b8e61c3c5bb0cee98bb45"
+        },
+        "date": 1760258104430,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "write_1GB",
+            "value": 1.3641102313995361,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_10GB",
+            "value": 5.521522045135498,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_100GB",
+            "value": 51.63004660606384,
+            "unit": "seconds"
+          },
+          {
+            "name": "write_40GB",
+            "value": 20.92974328994751,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_1GB",
+            "value": 0.9978628158569336,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_10GB",
+            "value": 4.53100061416626,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_100GB",
+            "value": 51.50546479225159,
+            "unit": "seconds"
+          },
+          {
+            "name": "read_40GB",
+            "value": 15.507949590682983,
             "unit": "seconds"
           }
         ]
