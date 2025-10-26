4f47d5da41eb21a7130c5984ede0ebb4fddb8de7
github-action-benchmark <github@users.noreply.github.com>
2025-10-26 10:31:08 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4b679ae9a43c3d6ff4aa5d744280dc5ef3aa5849

--- ARM/premium/time/list/data.js
+++ ARM/premium/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760869356580,
+  "lastUpdate": 1761474668050,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -1001,6 +1001,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": 497.04,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Vikas Bhansali",
+            "username": "vibhansa-msft",
+            "email": "64532198+vibhansa-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "4b679ae9a43c3d6ff4aa5d744280dc5ef3aa5849",
+          "message": "Updating release date (#2032)",
+          "timestamp": "2025-10-16T04:32:59Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/4b679ae9a43c3d6ff4aa5d744280dc5ef3aa5849"
+        },
+        "date": 1761474667753,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": 9.77,
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": 467.18,
             "unit": "seconds"
           }
         ]
