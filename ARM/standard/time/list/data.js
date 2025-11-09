98e7074ee243efb9692e607bca55b79c00efa37f
github-action-benchmark <github@users.noreply.github.com>
2025-11-09 12:45:10 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 421feb6dfe9ff7a89f7f224cb5af92f231539f18

--- ARM/standard/time/list/data.js
+++ ARM/standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762086915262,
+  "lastUpdate": 1762692310251,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -903,6 +903,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": 767.25,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "421feb6dfe9ff7a89f7f224cb5af92f231539f18",
+          "message": "Bump github.com/Azure/azure-sdk-for-go/sdk/storage/azdatalake from 1.4.2 to 1.4.3 (#2057)\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-11-07T08:58:35Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/421feb6dfe9ff7a89f7f224cb5af92f231539f18"
+        },
+        "date": 1762692310015,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": 18.83,
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": 738.02,
             "unit": "seconds"
           }
         ]
