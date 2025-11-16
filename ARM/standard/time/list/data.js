27701e45e7ebe2aedd1a9d28e4d956d7c5655660
github-action-benchmark <github@users.noreply.github.com>
2025-11-16 13:05:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d2e3c8a69629afeda7e9b0d63074460fddbf8ca0

--- ARM/standard/time/list/data.js
+++ ARM/standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762692310251,
+  "lastUpdate": 1763298316131,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -935,6 +935,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": 738.02,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Sourav Gupta",
+            "username": "souravgupta-msft",
+            "email": "98318303+souravgupta-msft@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d2e3c8a69629afeda7e9b0d63074460fddbf8ca0",
+          "message": "Adding mlperf scripts (#2061)",
+          "timestamp": "2025-11-12T12:22:03Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/d2e3c8a69629afeda7e9b0d63074460fddbf8ca0"
+        },
+        "date": 1763298315900,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": 19.49,
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": 751.5,
             "unit": "seconds"
           }
         ]
