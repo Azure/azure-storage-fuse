0000a7624086685b75f26bf538b6feef9fea0ff9
Srinivas Yeleti <syeleti@microsoft.com>
2025-11-28 05:24:42 +0000

    Add Write & Truncate Support

--- component/block_cache/block_cache.go
+++ component/block_cache/block_cache.go
@@ -76,22 +76,25 @@ type BlockCache struct {
 	prefetchOnOpen  bool            // Start prefetching on file open call instead of waiting for first read
 	consistency     bool            // Flag to indicate if strong data consistency is enabled
 	//	stream          *Stream
-	lazyWrite    bool           // Flag to indicate if lazy write is enabled
-	fileCloseOpt sync.WaitGroup // Wait group to wait for all async close operations to complete
+	lazyWrite              bool           // Flag to indicate if lazy write is enabled
+	fileCloseOpt           sync.WaitGroup // Wait group to wait for all async close operations to complete
+	maxFileSize            uint64         // Max file size supported by block cache
+	deferEmptyBlobCreation bool           // Flag to indicate if empty blob creation is deferred until file is closed
 }
 
 // Structure defining your config parameters
 type BlockCacheOptions struct {
-	BlockSize      float64 `config:"block-size-mb" yaml:"block-size-mb,omitempty"`
-	MemSize        uint64  `config:"mem-size-mb" yaml:"mem-size-mb,omitempty"`
-	TmpPath        string  `config:"path" yaml:"path,omitempty"`
-	DiskSize       uint64  `config:"disk-size-mb" yaml:"disk-size-mb,omitempty"`
-	DiskTimeout    uint32  `config:"disk-timeout-sec" yaml:"timeout-sec,omitempty"`
-	PrefetchCount  uint32  `config:"prefetch" yaml:"prefetch,omitempty"`
-	Workers        uint32  `config:"parallelism" yaml:"parallelism,omitempty"`
-	PrefetchOnOpen bool    `config:"prefetch-on-open" yaml:"prefetch-on-open,omitempty"`
-	Consistency    bool    `config:"consistency" yaml:"consistency,omitempty"`
-	CleanupOnStart bool    `config:"cleanup-on-start" yaml:"cleanup-on-start,omitempty"`
+	BlockSize              float64 `config:"block-size-mb" yaml:"block-size-mb,omitempty"`
+	MemSize                uint64  `config:"mem-size-mb" yaml:"mem-size-mb,omitempty"`
+	TmpPath                string  `config:"path" yaml:"path,omitempty"`
+	DiskSize               uint64  `config:"disk-size-mb" yaml:"disk-size-mb,omitempty"`
+	DiskTimeout            uint32  `config:"disk-timeout-sec" yaml:"timeout-sec,omitempty"`
+	PrefetchCount          uint32  `config:"prefetch" yaml:"prefetch,omitempty"`
+	Workers                uint32  `config:"parallelism" yaml:"parallelism,omitempty"`
+	PrefetchOnOpen         bool    `config:"prefetch-on-open" yaml:"prefetch-on-open,omitempty"`
+	Consistency            bool    `config:"consistency" yaml:"consistency,omitempty"`
+	CleanupOnStart         bool    `config:"cleanup-on-start" yaml:"cleanup-on-start,omitempty"`
+	DeferEmptyBlobCreation bool    `config:"defer-empty-blob-creation" yaml:"defer-empty-blob-creation,omitempty"`
 }
 
 const (
@@ -181,10 +184,18 @@ func (bc *BlockCache) Configure(_ bool) error {
 		return fmt.Errorf("config error in %s [%s]", bc.Name(), err.Error())
 	}
 
+	if config.IsSet(compName + ".defer-empty-blob-creation") {
+		bc.deferEmptyBlobCreation = conf.DeferEmptyBlobCreation
+	} else {
+		bc.deferEmptyBlobCreation = true
+	}
+
 	bc.blockSize = uint64(defaultBlockSize) * common.MbToBytes
 	if config.IsSet(compName + ".block-size-mb") {
 		bc.blockSize = uint64(conf.BlockSize * float64(common.MbToBytes))
 	}
+
+	bc.maxFileSize = bc.blockSize * MAX_BLOCKS
 
 	if config.IsSet(compName + ".mem-size-mb") {
 		bc.memSize = conf.MemSize * common.MbToBytes
@@ -217,10 +228,10 @@ func (bc *BlockCache) Configure(_ bool) error {
 		bc.prefetch = conf.PrefetchCount
 		if bc.prefetch == 0 {
 			bc.noPrefetch = true
-		} else if conf.PrefetchCount <= (MIN_PREFETCH * 2) {
-			log.Err("BlockCache::Configure : Prefetch count can not be less then %v", (MIN_PREFETCH*2)+1)
-			return fmt.Errorf("config error in %s [invalid prefetch count]", bc.Name())
-		}
+		} // else if conf.PrefetchCount <= (MIN_PREFETCH * 2) {
+		// 	log.Err("BlockCache::Configure : Prefetch count can not be less then %v", (MIN_PREFETCH*2)+1)
+		// 	return fmt.Errorf("config error in %s [invalid prefetch count]", bc.Name())
+		// }
 	}
 
 	bc.maxDiskUsageHit = false
@@ -275,8 +286,8 @@ func (bc *BlockCache) Configure(_ bool) error {
 		// }
 	}
 
-	log.Crit("BlockCache::Configure : block size %v, mem size %v, worker %v, prefetch %v, disk path %v, max size %v, disk timeout %v, prefetch-on-open %t, maxDiskUsageHit %v, noPrefetch %v, consistency %v, cleanup-on-start %t",
-		bc.blockSize, bc.memSize, bc.workers, bc.prefetch, bc.tmpPath, bc.diskSize, bc.diskTimeout, bc.prefetchOnOpen, bc.maxDiskUsageHit, bc.noPrefetch, bc.consistency, conf.CleanupOnStart)
+	log.Crit("BlockCache::Configure : block size %v, mem size %v, worker %v, prefetch %v, disk path %v, max size %v, disk timeout %v, prefetch-on-open %t, maxDiskUsageHit %v, noPrefetch %v, consistency %v, cleanup-on-start %t, defer-empty-blob-creation %v",
+		bc.blockSize, bc.memSize, bc.workers, bc.prefetch, bc.tmpPath, bc.diskSize, bc.diskTimeout, bc.prefetchOnOpen, bc.maxDiskUsageHit, bc.noPrefetch, bc.consistency, conf.CleanupOnStart, bc.deferEmptyBlobCreation)
 
 	return nil
 }
@@ -288,14 +299,15 @@ func (bc *BlockCache) GetAttr(options internal.GetAttrOptions) (*internal.ObjAtt
 	if err != nil {
 		return attr, err
 	}
+
 	// file stucture has more updated info than attribute cache/Azure storage, if the file is open
 	file, ok := checkFileExistsInOpen(options.Name)
 	if ok {
 		fileSize := atomic.LoadInt64(&file.size)
-		if fileSize != attr.Size {
-			// There has been a modification done on the file.
-			// Return new attribute with new file size.
-			// We dont want to update the value inside the attribute itself, as it changes the state of the attribute inside the attribute cache
+		if (fileSize != -1) && fileSize != attr.Size {
+			// There has been a modification done on the file. Return new attribute with new file size.
+			// We dont want to update the value inside the attribute itself, as it changes the state of the attribute
+			// inside the attribute cache
 			newattr := *attr
 			newattr.Size = fileSize
 			return &newattr, nil
@@ -316,31 +328,55 @@ func (bc *BlockCache) CreateFile(options internal.CreateFileOptions) (*handlemap
 
 	return bc.OpenFile(internal.OpenFileOptions{
 		Name:  options.Name,
-		Flags: os.O_RDWR | os.O_TRUNC, //TODO: Standard says to open in O_WRONLY|O_TRUNC due to the writeback cache I defaulted it, it shoudl be change in future
+		Flags: os.O_RDWR | os.O_CREATE,
 		Mode:  options.Mode,
 	})
 }
 
 // OpenFile: Create a handle for the file user has requested to open
 func (bc *BlockCache) OpenFile(options internal.OpenFileOptions) (*handlemap.Handle, error) {
-	log.Trace("BlockCache::OpenFile : name=%s, flags=%X, mode=%s", options.Name, options.Flags, options.Mode)
+	log.Trace("BlockCache::OpenFile : name=%s, flags=%s, mode=%s", options.Name, common.PrettyOpenFlags(options.Flags), options.Mode)
+
 	attr, err := bc.GetAttr(internal.GetAttrOptions{Name: options.Name})
 	if err != nil {
 		log.Err("BlockCache::OpenFile : Failed to get attr of %s [%s]", options.Name, err.Error())
 		return nil, err
 	}
 
-	f, firstOpen := getFileFromPath(options.Name)
+	handle := createFreshHandleForFile(options.Name, attr.Size, attr.Mtime, options.Flags)
+
+	// Get file object from the map or create a new one for this path.
+	f, firstOpen := getFileFromPath(handle)
 	f.mu.Lock()
 	defer f.mu.Unlock()
+
+	handle.IFObj = &blockCacheHandle{
+		file:            f,
+		patternDetector: newPatternDetector(),
+	}
+
 	if f.size == -1 {
 		f.size = attr.Size
+		f.sizeOnStorage = attr.Size
 	}
 
 	if options.Flags&os.O_TRUNC != 0 {
+		log.Debug("BlockCache::OpenFile : Truncating file %s on open", options.Name)
+
+		if !firstOpen {
+			// There are some open handles for this file, flush all the data before truncating.
+			err = f.flush(false /* takefilelock */)
+			if err != nil {
+				log.Err("BlockCache::OpenFile : Failed to flush file %s before truncating on open [%v]", options.Name, err)
+				deleteOpenHandleForFile(handle, false /* takeFileLock */)
+				return nil, err
+			}
+			releaseAllBuffersForFile(f)
+			f.blockList = newBlockList()
+		}
+
 		f.size = 0
-		// TODO: Release all existing buffers to this file
-		f.blockList = newBlockList()
+		f.synced = false
 	}
 
 	if f.size == 0 {
@@ -349,36 +385,40 @@ func (bc *BlockCache) OpenFile(options internal.OpenFileOptions) (*handlemap.Han
 	}
 
 	if f.size > 0 {
-		if (f.blockList.state == blockListNotRetrieved) && ((options.Flags&os.O_WRONLY != 0) || (options.Flags&os.O_RDWR != 0)) {
-			blkList, err := bc.NextComponent().GetCommittedBlockList(options.Name)
-			if err != nil {
-				log.Err("BlockCache::OpenFile : Failed to get block list of %s, first_open: %v, curOpenHandles: %d, [%v]",
-					options.Name, firstOpen, len(f.handles), err)
-				return nil, err
-			}
+		if (options.Flags&os.O_WRONLY != 0) || (options.Flags&os.O_RDWR != 0) {
+			if f.blockList.state == blockListNotRetrieved {
+				blkList, err := bc.NextComponent().GetCommittedBlockList(options.Name)
+				if err != nil {
+					log.Err("BlockCache::OpenFile : Failed to get block list of %s, first_open: %v, curOpenHandles: %d, [%v]",
+						options.Name, firstOpen, len(f.handles), err)
+					deleteOpenHandleForFile(handle, false /* takeFileLock */)
+					return nil, err
+				}
 
-			err = validateBlockList(blkList, f)
-			if err != nil {
-				log.Err("BlockCache::OpenFile : Invalid block list for file: %s, first_open: %v, curOpenHandles: %d,  [%v]",
-					options.Name, firstOpen, len(f.handles), err)
-				f.blockList.state = blockListInvalid
-				deleteFileIfNoOpenHandles(options.Name)
-				return nil, err
-			} else {
-				f.blockList.state = blockListValid
+				err = validateBlockList(blkList, f)
+				if err != nil {
+					log.Err("BlockCache::OpenFile : Invalid block list for file: %s, first_open: %v, curOpenHandles: %d,  [%v]",
+						options.Name, firstOpen, len(f.handles), err)
+					f.blockList.state = blockListInvalid
+					deleteOpenHandleForFile(handle, false /* takeFileLock */)
+					return nil, err
+				} else {
+					f.blockList.state = blockListValid
+				}
+			} else if f.blockList.state == blockListInvalid {
+				log.Err("BlockCache::OpenFile : Invalid block list for file: %s, first_open: %v, curOpenHandles: %d",
+					options.Name, firstOpen, len(f.handles))
+				deleteOpenHandleForFile(handle, false /* takeFileLock */)
+				return nil, fmt.Errorf("invalid block list for file: %s", options.Name)
 			}
-
 		} else {
 			updateBlockListForReadOnlyFile(f)
 		}
 	}
 
-	handle := createFreshHandleForFile(f.Name, f.size, attr.Mtime, options.Flags)
-	f.handles[handle] = struct{}{}
-	handle.IFObj = &blockCacheHandle{
-		file:            f,
-		patternDetector: newPatternDetector(),
-	}
+	// libfuse handler, is not sending the flush call if this is not set. So setting it by default.
+	// TODO: no need for this
+	handle.Flags.Set(handlemap.HandleFlagDirty)
 
 	return handle, nil
 }
@@ -387,7 +427,7 @@ func (bc *BlockCache) OpenFile(options internal.OpenFileOptions) (*handlemap.Han
 func (bc *BlockCache) ReadInBuffer(options *internal.ReadInBufferOptions) (int, error) {
 	bcHandle := options.Handle.IFObj.(*blockCacheHandle)
 
-	log.Trace("BlockCache::ReadInBuffer : name: %s, buf size: %d, offset: %d, handle: %d",
+	log.Debug("BlockCache::ReadInBuffer : name: %s, buf size: %d, offset: %d, handle: %d",
 		options.Handle.Path, len(options.Data), options.Offset, options.Handle.ID)
 
 	// we schedule read-ahead per handle, rather than per file, to support multiple handles reading concurrently
@@ -405,64 +445,170 @@ func (bc *BlockCache) ReadInBuffer(options *internal.ReadInBufferOptions) (int, 
 
 // WriteFile: Write to the local file
 func (bc *BlockCache) WriteFile(options *internal.WriteFileOptions) (int, error) {
-	log.Debug("BlockCache::WriteFile : name: %s, buf size: %d, offset: %d",
-		options.Handle.Path, len(options.Data), options.Offset)
+	bcHandle := options.Handle.IFObj.(*blockCacheHandle)
 
-	return 0, syscall.ENOTSUP
+	log.Debug("BlockCache::WriteFile : name: %s, buf size: %d, offset: %d, handle: %d",
+		options.Handle.Path, len(options.Data), options.Offset, options.Handle.ID)
+
+	err := bcHandle.file.write(options)
+	if err != nil {
+		log.Err("BlockCache::WriteFile : Failed to write file %s at offset %d, size %d [%v]",
+			options.Handle.Path, options.Offset, len(options.Data), err)
+	}
+
+	return len(options.Data), err
 }
 
+// TruncateFile: Truncate the file to specified size
+func (bc *BlockCache) TruncateFile(options internal.TruncateFileOptions) error {
+	log.Trace("BlockCache::TruncateFile : name: %s, size: %d", options.Name, options.NewSize)
+
+	if options.Handle == nil {
+		log.Info("BlockCache::TruncateFile : Handle is nil for file %s, Opening the file", options.Name)
+
+		truncHandle, err := bc.OpenFile(internal.OpenFileOptions{
+			Name:  options.Name,
+			Flags: os.O_RDWR,
+		})
+		if err != nil {
+			log.Err("BlockCache::TruncateFile : Failed to open file %s for truncate [%v]", options.Name, err)
+			return err
+		}
+
+		defer func() {
+			err := bc.ReleaseFile(internal.ReleaseFileOptions{
+				Handle: truncHandle,
+			})
+			if err != nil {
+				log.Err("BlockCache::TruncateFile : Failed to release handle for file %s after truncate [%v]", options.Name, err)
+			}
+		}()
+
+		options.Handle = truncHandle
+	}
+
+	bcHandle := options.Handle.IFObj.(*blockCacheHandle)
+
+	log.Debug("BlockCache::TruncateFile : name: %s, size: %d, handle: %d",
+		options.Handle.Path, options.NewSize, options.Handle.ID)
+
+	err := bcHandle.file.truncate(&options)
+	if err != nil {
+		log.Err("BlockCache::TruncateFile : Failed to truncate file %s to size %d [%v]",
+			options.Handle.Path, options.NewSize, err)
+		return err
+	}
+
+	return nil
+}
+
+// SyncFile: Sync the file to remote storage
+// Call comes when user-application calls fsync on the file descriptor
 func (bc *BlockCache) SyncFile(options internal.SyncFileOptions) error {
-	log.Trace("BlockCache::SyncFile : handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
+	bcHandle := options.Handle.IFObj.(*blockCacheHandle)
 
-	return syscall.ENOTSUP
+	log.Debug("BlockCache::SyncFile : handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
+
+	err := bcHandle.file.flush(true /* takefilelock */)
+	if err != nil {
+		log.Err("BlockCache::SyncFile : Failed to sync file %s [%v]", options.Handle.Path, err)
+		return err
+	}
+
+	return nil
 }
 
-// FlushFile: Flush the local file to storage
+// FlushFile: Call comes when user-application is closing the file descriptor.
+// There is a possibility that we get multiple flush calls for the same handle if user used dup2 or fork.
 func (bc *BlockCache) FlushFile(options internal.FlushFileOptions) error {
-	log.Trace("BlockCache::FlushFile : handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
-	deleteOpenHandleForFile(options.Handle)
-	freeList.debugListMustBeFull()
+	bcHandle := options.Handle.IFObj.(*blockCacheHandle)
+
+	log.Debug("BlockCache::FlushFile : handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
+
+	err := bcHandle.file.flush(true /* takefilelock */)
+	if err != nil {
+		log.Err("BlockCache::FlushFile : Failed to flush file %s [%v]", options.Handle.Path, err)
+		return err
+	}
 
 	return nil
 }
 
 // ReleaseFile: all the FD's corresponding to this handle were closed by application so release all the blocks
 // and submit back to the pool.
+// Flush the file before releasing, if some memory mapped file is still dirty..btw, we don't get the flush call from
+// FUSE if the backing fd of the map is already closed by the application.
 func (bc *BlockCache) ReleaseFile(options internal.ReleaseFileOptions) error {
 	log.Trace("BlockCache::ReleaseFile : handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
+
+	err := bc.FlushFile(internal.FlushFileOptions{
+		Handle: options.Handle,
+	})
+	if err != nil {
+		log.Err("BlockCache::ReleaseFile : Failed to flush file %s before release [%v]", options.Handle.Path, err)
+	}
+
+	deleteOpenHandleForFile(options.Handle, true /* takeFileLock */)
+	// freeList.debugListMustBeFull()
+	log.Debug("BlockCache::ReleaseFile : Released handle: %d, path: %s", options.Handle.ID, options.Handle.Path)
 	return nil
 }
 
 // DeleteFile: Invalidate the file in local cache.
 func (bc *BlockCache) DeleteFile(options internal.DeleteFileOptions) error {
 	log.Trace("BlockCache::DeleteFile : name: %s", options.Name)
-	return syscall.ENOTSUP
+
+	err := bc.NextComponent().DeleteFile(options)
+	if err != nil {
+		log.Err("BlockCache::DeleteFile : error  %s [%s]", options.Name, err.Error())
+		return err
+	}
+
+	return nil
 }
 
 // RenameFile: Invalidate the file in local cache.
 func (bc *BlockCache) RenameFile(options internal.RenameFileOptions) error {
 	log.Trace("BlockCache::RenameFile : src: %s -> dst: %s", options.Src, options.Dst)
 
-	return syscall.ENOTSUP
+	err := bc.NextComponent().RenameFile(options)
+	if err != nil {
+		log.Err("BlockCache::RenameFile : %s failed to rename file [%s]", options.Src, err.Error())
+		return err
+	}
+
+	return nil
 }
 
 // DeleteDir: Recursively invalidate the directory and its children
 func (bc *BlockCache) DeleteDir(options internal.DeleteDirOptions) error {
 	log.Trace("BlockCache::DeleteDir : name: %s", options.Name)
 
-	return syscall.ENOTSUP
+	err := bc.NextComponent().DeleteDir(options)
+	if err != nil {
+		log.Err("BlockCache::DeleteDir : %s failed", options.Name)
+		return err
+	}
+
+	return nil
 }
 
 // RenameDir: Recursively invalidate the source directory and its children
 func (bc *BlockCache) RenameDir(options internal.RenameDirOptions) error {
 	log.Trace("BlockCache::RenameDir : src: %s -> dst: %s", options.Src, options.Dst)
 
-	return syscall.ENOTSUP
+	err := bc.NextComponent().RenameDir(options)
+	if err != nil {
+		log.Err("BlockCache::RenameDir : error %s [%s]", options.Src, err.Error())
+		return err
+	}
+
+	return nil
 }
 
 func (bc *BlockCache) StatFs() (*syscall.Statfs_t, bool, error) {
 	log.Trace("BlockCache::StatFS")
-	return nil, false, syscall.ENOTSUP
+	return &syscall.Statfs_t{}, true, nil
 }
 
 // ------------------------- Factory -------------------------------------------
@@ -507,4 +653,7 @@ func init() {
 	strongConsistency := config.AddBoolFlag("block-cache-strong-consistency", false, "Enable strong data consistency for block cache.")
 	config.BindPFlag(compName+".consistency", strongConsistency)
 
+	// New flags go here
+	deferEmptyBlobCreation := config.AddBoolFlag("block-cache-defer-empty-file-creation", true, "When a new file is created, defer its creation on the remote storage until data is actually written to it. file is created on remote storage when the handle is closed/fsynced.")
+	config.BindPFlag(compName+".defer-empty-blob-creation", deferEmptyBlobCreation)
 }
