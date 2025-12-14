b462969b61d089a90a9c324ab03255ebed196ba2
github-action-benchmark <github@users.noreply.github.com>
2025-12-14 13:19:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d8a31a5066f5f064fdce5de9fbf44006bf0693d5

--- ARM/standard/time/list/data.js
+++ ARM/standard/time/list/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765111967219,
+  "lastUpdate": 1765718380722,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -1063,6 +1063,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "delete_100k_files",
             "value": 781.2,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "syeleti-msft",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d8a31a5066f5f064fdce5de9fbf44006bf0693d5",
+          "message": "Fix linting issues (#2087)",
+          "timestamp": "2025-12-12T08:13:45Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/d8a31a5066f5f064fdce5de9fbf44006bf0693d5"
+        },
+        "date": 1765718380484,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "list_100k_files",
+            "value": 23.44,
+            "unit": "seconds"
+          },
+          {
+            "name": "delete_100k_files",
+            "value": 882.83,
             "unit": "seconds"
           }
         ]
