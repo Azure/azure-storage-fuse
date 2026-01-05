77d4cc5baf94e2ca93d75024dffce708cede7976
github-action-benchmark <github@users.noreply.github.com>
2026-01-05 17:28:33 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for af160039bb50ceedeadb8e35d831a6d352de1395

---
+++ X86/premium/block_cache/time/write/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767634113053,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "id": "af160039bb50ceedeadb8e35d831a6d352de1395",
+          "message": "modify benchmarks",
+          "timestamp": "2026-01-05T09:12:19Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/af160039bb50ceedeadb8e35d831a6d352de1395"
+        },
+        "date": 1767634112800,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write_kernel_cache",
+            "value": 0.5018373398093333,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 0.482565629353,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
