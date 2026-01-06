4555221fdccaf9fe0c07af8e4fb493ecd6d25fb1
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 10:19:54 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 45325c0ae0a6e633557687f3807253c8dddc7694

--- X86/standard/file_cache/bandwidth/write/data.js
+++ X86/standard/file_cache/bandwidth/write/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767637537024,
+  "lastUpdate": 1767694794405,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "sequential_write_16_threads",
             "value": 5658.771484375,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "id": "45325c0ae0a6e633557687f3807253c8dddc7694",
+          "message": "modify benchmarks",
+          "timestamp": "2026-01-05T09:12:19Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/45325c0ae0a6e633557687f3807253c8dddc7694"
+        },
+        "date": 1767694794069,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write_kernel_cache",
+            "value": 1378.62890625,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 1232.658203125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_write_4_threads",
+            "value": 4604.571614583333,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_write_16_threads",
+            "value": 5786.439453125,
             "unit": "MiB/s"
           }
         ]
