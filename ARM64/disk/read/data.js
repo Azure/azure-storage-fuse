a8ba0071bfc9da01bd36e10f31401522cbb4ca72
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 11:23:32 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c1a8d67c576acfbf5e92cc8abb649364554c7ecc

---
+++ ARM64/disk/read/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767698612358,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "id": "c1a8d67c576acfbf5e92cc8abb649364554c7ecc",
+          "message": "modify benchmarks",
+          "timestamp": "2026-01-05T09:12:19Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/c1a8d67c576acfbf5e92cc8abb649364554c7ecc"
+        },
+        "date": 1767698612114,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read_directio",
+            "value": 2571.65234375,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
