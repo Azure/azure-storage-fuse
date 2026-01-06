1ad0102ef134fdc6dcba83f7f75eb11682489470
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 10:19:55 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 45325c0ae0a6e633557687f3807253c8dddc7694

--- X86/standard/file_cache/time/write/data.js
+++ X86/standard/file_cache/time/write/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767637538360,
+  "lastUpdate": 1767694795774,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "sequential_write_16_threads",
             "value": 2.82640877144,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "Srinivas Yeleti",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "id": "45325c0ae0a6e633557687f3807253c8dddc7694",
+          "message": "modify benchmarks",
+          "timestamp": "2026-01-05T09:12:19Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/45325c0ae0a6e633557687f3807253c8dddc7694"
+        },
+        "date": 1767694795525,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write_kernel_cache",
+            "value": 0.726334090187,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_directio",
+            "value": 0.8116195215173333,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_4_threads",
+            "value": 0.8712247040559999,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_write_16_threads",
+            "value": 2.7627527524340003,
             "unit": "milliseconds"
           }
         ]
