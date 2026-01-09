446f5da15149304940ed01d95637d2e3d035fe16
syeleti-msft <syeleti@microsoft.com>
2026-01-09 15:12:45 +0530

    Remove getting size from statfs (#2083)
    Co-authored-by: Vikas Bhansali <64532198+vibhansa-msft@users.noreply.github.com>

--- common/util.go
+++ common/util.go
@@ -471,35 +471,6 @@ func GetUsage(path string) (float64, error) {
 	return currSize, nil
 }
 
-var currentUID int = -1
-
-// GetDiskUsageFromStatfs: Current disk usage of temp path
-func GetDiskUsageFromStatfs(path string) (float64, float64, error) {
-	// We need to compute the disk usage percentage for the temp path
-	var stat syscall.Statfs_t
-	err := syscall.Statfs(path, &stat)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	if currentUID == -1 {
-		currentUID = os.Getuid()
-	}
-
-	var availableSpace uint64
-	if currentUID == 0 {
-		// Sudo  has mounted
-		availableSpace = stat.Bfree * uint64(stat.Frsize)
-	} else {
-		// non Sudo has mounted
-		availableSpace = stat.Bavail * uint64(stat.Frsize)
-	}
-
-	totalSpace := stat.Blocks * uint64(stat.Frsize)
-	usedSpace := float64(totalSpace - availableSpace)
-	return usedSpace, float64(usedSpace) / float64(totalSpace) * 100, nil
-}
-
 func GetFuseMinorVersion() int {
 	var out bytes.Buffer
 	cmd := exec.Command("fusermount3", "--version")
