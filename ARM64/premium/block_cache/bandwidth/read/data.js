d70575b49c369d9196475cf955d3f25e9821ceb5
github-action-benchmark <github@users.noreply.github.com>
2026-01-14 20:10:07 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for cd2cb4303028ac433d46f7284e70869eb529a19e

---
+++ ARM64/premium/block_cache/bandwidth/read/data.js
@@ -0,0 +1,65 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768421407845,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "syeleti-msft",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "cd2cb4303028ac433d46f7284e70869eb529a19e",
+          "message": "Add parallel file writes to different files fio configs (#2096)",
+          "timestamp": "2026-01-14T13:24:28Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/cd2cb4303028ac433d46f7284e70869eb529a19e"
+        },
+        "date": 1768421406241,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read_small_file",
+            "value": 3624.345703125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 3765.4736328125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read",
+            "value": 3457.0068359375,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read",
+            "value": 13.53515625,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_4_threads",
+            "value": 9463.080078125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_16_threads",
+            "value": 9582.4072265625,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_4_threads",
+            "value": 52.78125,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
