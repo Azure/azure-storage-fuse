ee20a234418e98062cee5cc017e494fd809c0478
github-action-benchmark <github@users.noreply.github.com>
2026-01-14 13:58:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for cd2cb4303028ac433d46f7284e70869eb529a19e

---
+++ X86/standard/block_cache/time/read/data.js
@@ -0,0 +1,65 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768399105406,
+  "repoUrl": "https://github.com/Azure/azure-storage-fuse",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "syeleti-msft",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "cd2cb4303028ac433d46f7284e70869eb529a19e",
+          "message": "Add parallel file writes to different files fio configs (#2096)",
+          "timestamp": "2026-01-14T13:24:28Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/cd2cb4303028ac433d46f7284e70869eb529a19e"
+        },
+        "date": 1768399105141,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read_small_file",
+            "value": 0.28560338323400003,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 0.216300925104,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read",
+            "value": 0.30428120339799997,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read",
+            "value": 110.621578316176,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_4_threads",
+            "value": 0.503336151318,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "sequential_read_16_threads",
+            "value": 1.364979255542,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "random_read_4_threads",
+            "value": 111.670116146704,
+            "unit": "milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
