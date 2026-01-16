f677250341aef73f0f8b02878aeb2f92405e6062
github-action-benchmark <github@users.noreply.github.com>
2026-01-16 15:19:23 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for cd2cb4303028ac433d46f7284e70869eb529a19e

--- ARM64/disk/read/data.js
+++ ARM64/disk/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768554986355,
+  "lastUpdate": 1768576763895,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -80,6 +80,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "sequential_read_directio",
             "value": 11595.1875,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "syeleti-msft",
+            "username": "syeleti-msft",
+            "email": "syeleti@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "cd2cb4303028ac433d46f7284e70869eb529a19e",
+          "message": "Add parallel file writes to different files fio configs (#2096)",
+          "timestamp": "2026-01-14T13:24:28Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/cd2cb4303028ac433d46f7284e70869eb529a19e"
+        },
+        "date": 1768576763657,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read_directio",
+            "value": 11505.6171875,
             "unit": "MiB/s"
           }
         ]
