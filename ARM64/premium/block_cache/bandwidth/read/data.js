b49cdc38c2f7916024b5e67a7566f35e6e83fa55
github-action-benchmark <github@users.noreply.github.com>
2026-02-02 08:00:19 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 658ec3c26e873921a02269a849be749085a4791e

--- ARM64/premium/block_cache/bandwidth/read/data.js
+++ ARM64/premium/block_cache/bandwidth/read/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769329519011,
+  "lastUpdate": 1770019219526,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -398,6 +398,63 @@ window.BENCHMARK_DATA = {
           {
             "name": "random_read_4_threads",
             "value": 53.6865234375,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Copilot",
+            "username": "Copilot",
+            "email": "198982749+Copilot@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "658ec3c26e873921a02269a849be749085a4791e",
+          "message": "Disable FIO read tests for filecache mode and add custom 100GB direct I/O test (#2106)\n\nCo-authored-by: copilot-swe-agent[bot] <198982749+Copilot@users.noreply.github.com>\nCo-authored-by: syeleti-msft <172502481+syeleti-msft@users.noreply.github.com>",
+          "timestamp": "2026-01-30T03:38:01Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/658ec3c26e873921a02269a849be749085a4791e"
+        },
+        "date": 1770019217882,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_read_small_file",
+            "value": 3666.0107421875,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_small_file",
+            "value": 3554.34765625,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read",
+            "value": 3666.1767578125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read",
+            "value": 13.7705078125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_4_threads",
+            "value": 9287.138671875,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "sequential_read_16_threads",
+            "value": 9814.3037109375,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "random_read_4_threads",
+            "value": 52.283203125,
             "unit": "MiB/s"
           }
         ]
