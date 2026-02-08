3f990899a85b5a8dd8accc741ecad81a9dfbb387
github-action-benchmark <github@users.noreply.github.com>
2026-02-08 07:52:15 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 658ec3c26e873921a02269a849be749085a4791e

--- ARM64/standard/file_cache/bandwidth/write/data.js
+++ ARM64/standard/file_cache/bandwidth/write/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770016645447,
+  "lastUpdate": 1770537135269,
   "repoUrl": "https://github.com/Azure/azure-storage-fuse",
   "entries": {
     "Benchmark": [
@@ -255,6 +255,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "seq_write_parallel_16_files",
             "value": 3822.8564453125,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Copilot",
+            "username": "Copilot",
+            "email": "198982749+Copilot@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "658ec3c26e873921a02269a849be749085a4791e",
+          "message": "Disable FIO read tests for filecache mode and add custom 100GB direct I/O test (#2106)\n\nCo-authored-by: copilot-swe-agent[bot] <198982749+Copilot@users.noreply.github.com>\nCo-authored-by: syeleti-msft <172502481+syeleti-msft@users.noreply.github.com>",
+          "timestamp": "2026-01-30T03:38:01Z",
+          "url": "https://github.com/Azure/azure-storage-fuse/commit/658ec3c26e873921a02269a849be749085a4791e"
+        },
+        "date": 1770537134850,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "sequential_write",
+            "value": 827.8828125,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "seq_write_parallel_16_files",
+            "value": 3763.2353515625,
             "unit": "MiB/s"
           }
         ]
